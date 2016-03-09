a349bfd38e38484140717963a22b865bb9e97a32
Mattis Kaemmerer <M8is@live.de>
2016-03-09 18:43:30 +0100

    YAMLReader can now run a function on each data file content and return the accumulated results

--- scripts/benchmark/datareader.py
+++ scripts/benchmark/datareader.py
@@ -3,7 +3,7 @@ from os import listdir
 from os.path import isfile, join
 
 class YAMLReader:
-        def readAllFixingCommits(datapath, verbose):
+        def readAllFrom(datapath, verbose):
             datafiles = [join(datapath, file) for file in listdir(datapath) if isfile(join(datapath, file)) and file.endswith(".yml")]
 
             data = []
@@ -11,16 +11,37 @@ class YAMLReader:
                 stream = open(file, 'r')
                 try:
                     yamlContent = yaml.load(stream)
-                    fixingCommit = yamlContent["fix"]["commit"]
-                    data.append(fixingCommit)
+                    data.append(yamlContent)
                     if verbose:
-                        print(fixingCommit)
+                        print("Successfully added " + file)
                 finally:
                     stream.close()
             return data
 
+        def onAllDataDo(function, datapath, verbose):
+            datafiles = [join(datapath, file) for file in listdir(datapath) if isfile(join(datapath, file)) and file.endswith(".yml")]
+
+            result = []
+            for file in datafiles:
+                stream = open(file, 'r')
+                try:
+                    yamlContent = yaml.load(stream)
+                    result.append(function(yamlContent))
+                    if verbose:
+                        print("Successfully added " + file)
+                finally:
+                    stream.close()
+            return result
+                    
+class example:
+    def f(data):
+        print("f was called")
+
 # example usage:
-#datapath = '/home/pi/Documents/Eko/MUBench/data'
-#fixingCommits = YAMLReader.readAllFixingCommits(datapath, True)
+# datapath = r'<path>\MUBench\data'
+# data = YAMLReader.readAllFrom(datapath, True)
 
-
+# example usage:
+datapath = r'<path>\MUBench\data'
+results = YAMLReader.onAllDataDo(example.f, datapath, True)
+print(results)
