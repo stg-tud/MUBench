2ec5029e1528c8b926aa73cb1b5fa1fc263a8c1d
Mattis Manfred Kaemmerer <M8is@live.de>
2016-03-10 15:16:38 +0100

    checkout and datareader are now modules (they can be imported)

--- scripts/benchmark/datareader.py
+++ scripts/benchmark/datareader.py
@@ -2,46 +2,41 @@ import yaml
 from os import listdir
 from os.path import isfile, join
 
-class YAMLReader:
-        def readAllFrom(datapath, verbose):
-            datafiles = [join(datapath, file) for file in listdir(datapath) if isfile(join(datapath, file)) and file.endswith(".yml")]
+def readAllFrom(datapath, verbose):
+    datafiles = [join(datapath, file) for file in listdir(datapath) if isfile(join(datapath, file)) and file.endswith(".yml")]
 
-            data = []
-            for file in datafiles:
-                stream = open(file, 'r')
-                try:
-                    yamlContent = yaml.load(stream)
-                    data.append(yamlContent)
-                    if verbose:
-                        print("Successfully added " + file)
-                finally:
-                    stream.close()
-            return data
+    data = []
+    for file in datafiles:
+        stream = open(file, 'r')
+        try:
+            yamlContent = yaml.load(stream)
+            data.append(yamlContent)
+            if verbose:
+                print("Successfully added " + file)
+        finally:
+            stream.close()
+    return data
 
-        def onAllDataDo(function, datapath, verbose):
-            datafiles = [join(datapath, file) for file in listdir(datapath) if isfile(join(datapath, file)) and file.endswith(".yml")]
+def onAllDataDo(function, datapath, verbose):
+    datafiles = [join(datapath, file) for file in listdir(datapath) if isfile(join(datapath, file)) and file.endswith(".yml")]
 
-            result = []
-            for file in datafiles:
-                stream = open(file, 'r')
-                try:
-                    yamlContent = yaml.load(stream)
-                    result.append(function(yamlContent))
-                    if verbose:
-                        print("Successfully added " + file)
-                finally:
-                    stream.close()
-            return result
-                    
-class example:
-    def f(data):
-        print("f was called")
+    result = []
+    for file in datafiles:
+        stream = open(file, 'r')
+        try:
+            yamlContent = yaml.load(stream)
+            result.append(function(yamlContent))
+            if verbose:
+                print("Successfully added " + file)
+        finally:
+            stream.close()
+    return result
+                
+# example usage:
+#datapath = r'<path>\MUBench\data'
+#data = YAMLReader.readAllFrom(datapath, True)
 
 # example usage:
-# datapath = r'<path>\MUBench\data'
-# data = YAMLReader.readAllFrom(datapath, True)
-
-# example usage:
-datapath = r'<path>\MUBench\data'
-results = YAMLReader.onAllDataDo(example.f, datapath, True)
-print(results)
+#datapath = r'<path>\MUBench\data'
+#results = YAMLReader.onAllDataDo(example.f, datapath, True)
+#print(results)
