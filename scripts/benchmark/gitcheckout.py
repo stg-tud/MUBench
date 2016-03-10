9d6cfe798ab62f5815c7f81740701baf708f4cb4
Mattis Manfred Kaemmerer <M8is@live.de>
2016-03-10 09:09:02 +0100

    Improved gitcheckout by using the recently extended data and made gitcheckout.sh obsolete

--- scripts/benchmark/gitcheckout.py
+++ scripts/benchmark/gitcheckout.py
@@ -1,46 +1,58 @@
-import sys
-import subprocess
+from os import listdir
+from os import mkdir
+from os.path import join
+from os.path import exists
+from os.path import isdir
 from subprocess import CalledProcessError
 from subprocess import check_output
-from os.path import join
-from os.path import dirname
-from os.path import realpath
 
-class gitCheckout:
-    def checkoutMisuse(self, fixing_commit, dirtarget, verbose):
-        repository, unused, commit_hash = fixing_commit.rsplit('/', 2)
-        commit_hash += "^1"
+class gitcheckout:
+    def checkout_misuse(repository, fixing_commit, dirtarget, verbose):
+        parent_of_fixing_commit = fixing_commit + '~1'
         
         if verbose:
             print("Checking out misuse to " + dirtarget)
             print("Repository: " + repository)
-            print("Commit hash: " + commit_hash)
+            print("Commit: " + parent_of_fixing_commit)
             
-        gitCheckout().checkout(repository, commit_hash, dirtarget)
-        
-    def checkoutFix(self, fixing_commit, dirtarget, verbose):
-        repository, unused, commit_hash = fixing_commit.rsplit('/', 2)
-        
+        gitcheckout.checkout(repository, parent_of_fixing_commit, dirtarget, verbose)
+    
+    def checkout_fix(repository, fixing_commit, dirtarget, verbose):
+              
         if verbose:
             print("Checking out fix to " + dirtarget)
             print("Repository: " + repository)
-            print("Commit hash: " + commit_hash)
+            print("Commit: " + fixing_commit)
             
-        gitCheckout().checkout(repository, commit_hash, dirtarget)
+        gitcheckout.checkout(repository, fixing_commit, dirtarget, verbose)
 
-    def checkout(self, repository, commit_hash, dirtarget):
+    def checkout(repository, commit, dirtarget, verbose):
         try:
-            gitcheckout = join(dirname(realpath(__file__)), 'gitcheckout.sh')
-            check_output([gitcheckout, repository, commit_hash, dirtarget],
-            executable=gitcheckout, stderr=subprocess.STDOUT)
+            if not exists(dirtarget):
+                mkdir(dirtarget)
+            
+            if isdir(dirtarget) and (listdir(dirtarget)==[]):
+                # Make sure no shell injection happens here!
+                # For more detail go to https://docs.python.org/3/library/subprocess.html#security-considerations                
+                check_output(['git', 'clone', repository, dirtarget], shell=True) 
+                check_output(['git', '--git-dir=' + join(dirtarget, '.git'), '--work-tree=' + dirtarget, 'checkout', commit], shell=True)
+            else:
+                raise ValueError('{0} is an empty directory!'.format(dirtarget))
+            
         except CalledProcessError as e:
             print("Git checkout failed: ")
+            print(e)
             print(e.output.decode('unicode_escape'))
+        except ValueError as e:
+            print("Git checkout failed: ")
+            print(e)
         else:
-            print("Git checkout successful.")
+            if verbose:
+                print("Git checkout successful.")
 
 # example usage
 from tempfile import mkdtemp
-commitid = 'https://github.com/google/closure-compiler/commit/67289ae4cbaba3ae70cd2e8fb92f3f2898039dfb'
-gitCheckout().checkoutMisuse(commitid, mkdtemp(suffix='_misuse'), True)
-gitCheckout().checkoutFix(commitid, mkdtemp(suffix='_fix'), True) 
+repository = r'https://github.com/google/closure-compiler'
+commit = '67289ae4cbaba3ae70cd2e8fb92f3f2898039dfb'
+gitcheckout.checkout_misuse(repository, commit, mkdtemp(suffix='_misuse'), True)
+gitcheckout.checkout_fix(repository, commit, mkdtemp(suffix='_fix'), True)
