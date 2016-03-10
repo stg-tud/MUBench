a93ce6f949210be179622c0f2ed8e64235256fb7
Mattis Manfred Kaemmerer <M8is@live.de>
2016-03-10 21:22:12 +0100

    Implemented analysis prototype to check for errors in the current infrastructure

---
+++ scripts/benchmark/benchmark.py
@@ -0,0 +1,47 @@
+from tempfile import mkdtemp
+from os.path import join
+from shutil import rmtree
+from checkout import checkout
+from checkout import checkout_parent
+from datetime import datetime
+
+def analyze(misuse):
+    try:
+        fix = misuse["fix"]
+        repository = fix["repository"]
+        
+        tmpdir = mkdtemp()
+        misusedir = join(tmpdir, "misuse")
+        fixdir = join(tmpdir, "fix")
+        
+        #checkout_parent(repository["type"], repository["url"], fix["revision"], misusedir, True)
+        #checkout(repository["type"], repository["url"], fix["revision"], fixdir, True)
+        
+        # TODO: run actual analysis here
+        result = None
+    
+        try:
+            rmtree(tmpdir)
+        except PermissionError as e:
+            print("Cleanup could not be completed for this repository: ")
+            print(e)
+        else:
+            print("Cleanup successful")
+
+        return result
+    except Exception as e:
+        # using str(e) would fail for unicode exceptions :/ 
+        return "Error: {} in {}".format(repr(e), misuse.get("project"))
+
+import datareader
+from pprint import pprint
+
+start_time = datetime.now()
+results = datareader.onAllDataDo(analyze, r'C:\Users\Mattis\Documents\Eko\MUBench\data', True)
+end_time = datetime.now()
+
+print("================================================")
+print("Analysis finished! Total time: {}".format(str(end_time - start_time)))
+print("Analysis results: ")
+pprint(results)
+print("================================================")
