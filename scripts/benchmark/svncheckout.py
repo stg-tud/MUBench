93382fb2dee7c07e55f8dd1b90c4017dce7fcc53
Mattis Manfred Kaemmerer <M8is@live.de>
2016-03-10 09:46:05 +0100

    Improved svncheckout by using the recently extended data and made svncheckout.sh obsolete

--- scripts/benchmark/svncheckout.py
+++ scripts/benchmark/svncheckout.py
@@ -1,45 +1,58 @@
-import sys
-import subprocess
+from os import listdir
+from os import mkdir
+from os.path import join
+from os.path import exists
+from os.path import isdir
 from subprocess import CalledProcessError
 from subprocess import check_output
-from os.path import join
-from os.path import dirname
-from os.path import realpath
 
-class svnCheckout:
-    def checkoutMisuse(self, fixing_commit, dirtarget, verbose):
-        repository, revision = '' #TODO
-        
+class svncheckout:
+    def checkout_misuse(repository, revision, dirtarget, verbose):
+        revision = str(revision-1)
+
         if verbose:
             print("Checking out misuse to " + dirtarget)
             print("Repository: " + repository)
             print("Revision: " + revision)
             
-        svnCheckout().checkout(repository, revision, dirtarget)
+        svncheckout.checkout(repository, revision, dirtarget, verbose)
         
-    def checkoutFix(self, fixing_commit, dirtarget, verbose):
-        repository, revision = '' #TODO
-        
+    def checkout_fix(repository, revision, dirtarget, verbose):
+        revision = str(revision)
+
         if verbose:
             print("Checking out fix to " + dirtarget)
             print("Repository: " + repository)
             print("Revision: " + revision)
             
-        svnCheckout().checkout(repository, commit_hash, dirtarget)
+        svncheckout.checkout(repository, revision, dirtarget, verbose)
 
-    def checkout(self, repository, revision, dirtarget):
+    def checkout(repository, revision, dirtarget, verbose):
         try:
-            svncheckout = join(dirname(realpath(__file__)), 'svncheckout.sh')
-            check_output([svncheckout, repository, revision, dirtarget],
-            executable=svncheckout, stderr=subprocess.STDOUT)
+            if not exists(dirtarget):
+                mkdir(dirtarget)
+            
+            if isdir(dirtarget) and (listdir(dirtarget)==[]):
+                # Make sure no shell injection happens here!
+                # For more detail go to https://docs.python.org/3/library/subprocess.html#security-considerations                
+                check_output(['svn', 'checkout', '--revision', str(revision), repository, dirtarget], shell=True)
+            else:
+                raise ValueError('{0} is an empty directory!'.format(dirtarget))
+            
         except CalledProcessError as e:
             print("SVN checkout failed: ")
+            print(e)
             print(e.output.decode('unicode_escape'))
+        except ValueError as e:
+            print("SVN checkout failed: ")
+            print(e)
         else:
-            print("SVN checkout successful.")
+            if verbose:
+                print("SVN checkout successful.")
 
 # example usage
 from tempfile import mkdtemp
-revision_url = ''
-svnCheckout().checkoutMisuse(revision_url, mkdtemp(suffix='_misuse'), True)
-svnCheckout().checkoutFix(revision_url, mkdtemp(suffix='_fix'), True) 
+repository = 'svn://svn.code.sf.net/p/genoviz/code/trunk'
+revision = 4273
+svncheckout.checkout_misuse(repository, revision, mkdtemp(suffix='_misuse'), True)
+svncheckout.checkout_fix(repository, revision, mkdtemp(suffix='_fix'), True) 
