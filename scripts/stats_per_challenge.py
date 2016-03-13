4dbd8c2025fb1c8f6710827de12bd508e80a6091
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-03-13 23:20:11 +0900

    Add script for challenge statistics.

---
+++ scripts/stats_per_challenge.py
@@ -0,0 +1,37 @@
+#
+# Prints statistics about the API misuses per characteristic.
+#
+from __future__ import division
+import os
+import yaml
+from sets import Set
+
+rootpath = ".."
+datapath = os.path.join(rootpath, "data")
+
+print "Scanning misuses in '%s'" % datapath
+
+statistics = {}
+
+for filename in os.listdir(datapath):
+    if filename.endswith(".yml"):
+        filename = os.path.join(datapath, filename)
+        file = open(filename, 'r')
+        try:
+            misuse = yaml.load(file)
+
+            if "challenges" in misuse:
+                challs = misuse["challenges"]
+                for chall in challs:
+                    stat = statistics.get(chall, 0)
+                    stat += 1
+                    statistics[chall] = stat
+        
+        finally:
+            file.close()
+
+print
+print "%25s %7s" % ("Challenge", "Misuses")
+for statname in statistics:
+    print "%25s %7d" % (statname, statistics[statname])
+
