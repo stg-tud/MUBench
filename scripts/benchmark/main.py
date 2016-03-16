54a1db1425b67ba7a57c36fc90f51aebb5fbf09d
Mattis Manfred Kaemmerer <M8is@live.de>
2016-03-16 10:01:12 +0100

    Added a main module to run the benchmark and evaluate the results

---
+++ scripts/benchmark/main.py
@@ -0,0 +1,21 @@
+import datetime
+
+from benchmark import analyze
+from datareader import on_all_data_do
+from results import evaluate_results
+from settings import DATA_PATH
+
+start_time = datetime.now()
+results = on_all_data_do(analyze, DATA_PATH, True)
+end_time = datetime.now()
+
+print("================================================")
+print("Analysis finished! Total time: {}".format(str(end_time - start_time)))
+print("================================================")
+
+print("Evaluating results...")
+evaluation_start_time = datetime.now()
+evaluate_results()
+end_time = datetime.now()
+print("Evaluation time: {} ; Complete benchmark time: {}".format(str(end_time - evaluation_start_time),
+                                                                 str(end_time - start_time)))
