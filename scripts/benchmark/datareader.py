0753b7a5e09603fdd59737018cccfbd673505973
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-03-18 09:06:00 +0100

    Replaced example in datareader with proper documentation

--- scripts/benchmark/datareader.py
+++ scripts/benchmark/datareader.py
@@ -6,6 +6,12 @@ from settings import *
 
 
 def on_all_data_do(function):
+    """
+    :param function: The function to execute on each data point
+    :rtype: list
+    :return: A list of the results of each function execution
+    """
+
     datafiles = [join(DATA_PATH, file) for file in listdir(DATA_PATH) if
                  isfile(join(DATA_PATH, file)) and file.endswith(".yml")]
 
@@ -25,12 +31,3 @@ def on_all_data_do(function):
                 print("------------------------------------------------------------")
 
     return result
-
-# example usage:
-# from pprint import pprint
-#
-# def f(file, data):
-#    return "({} -> {})".format(file, data)
-#
-# results = on_all_data_do(f)
-# pprint(results)
