ae804d673045570276d5b55e2c25128a7e4f569e
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-03-18 09:43:10 +0100

    Added some documentation to settings.py

--- scripts/benchmark/settings.py
+++ scripts/benchmark/settings.py
@@ -1,9 +1,14 @@
-# These settings are shared between all benchmark modules
+from os.path import join
 
-DATA_PATH = r"<...>\MUBench\data"  # path to the data folder
+# These settings are shared between all modules
+# IMPORTANT: always import settings and use settings like settings.DATA_PATH!
+# Doing it this way allows tests to alter these values
+
+DATA_PATH = r"E:\Eko\MUBench\data"  # path to the data folder
 FILE_BENCHMARK_RESULT = r"<result>"  # where the final result will be written
 RESULTS_PATH = r"<output_path>"  # used for saving intermediate results
 TEMP_SUBFOLDER = "<subfolder>"  # used as subfolder in the temp path (will show up in misuse detection results)
 MISUSE_DETECTOR = r"<path_to_jar>"  # path to the misuse detector to benchmark (must be an executable .jar)
 FILE_DETECTOR_RESULT = "finalAnomalies.txt"  # result written by the misuse detector
+LOG_FILE = join(RESULTS_PATH, "log.txt")  # errors will be logged into this file
 VERBOSE = True  # prints more information to console if True
