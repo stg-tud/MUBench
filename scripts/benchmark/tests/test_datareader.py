2f77808947d5b1408b13b393e542130d37262c40
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-03-18 08:52:16 +0100

    Added test stubs for datareader

---
+++ scripts/benchmark/tests/test_datareader.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class DatareaderTest(unittest.TestCase):
+    def test_on_all_data_do(self):
+        pass  # TODO
+
+    def test_return_values(self):
+        pass  # TODO
+
+if __name__ == '__main__':
+    unittest.main()
