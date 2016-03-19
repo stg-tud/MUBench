7e9f8e3f7f8f29aa256dbb004196c87c9ee0505f
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-03-19 21:56:41 +0100

    Improved result evaluation

--- scripts/benchmark/results.py
+++ scripts/benchmark/results.py
@@ -12,25 +12,30 @@ def evaluate_single_result(file_data, data_content):
         file_result = join(dir_result, settings.FILE_DETECTOR_RESULT)
         print("Evaluating result {} against data {}".format(file_result, file_data))
 
+        fix_filename = data_content["fix"]["files"][0]["name"]
+        fix_filename = normpath(fix_filename)
+        if "trunk" in fix_filename:
+            # cut everything including trunk folder, then use [1:] to cut the leading slash
+            fix_filename = fix_filename.split("trunk", 1)[1][1:]
+
+        print("Looking for file {}".format(fix_filename))
+
         if exists(file_result):
             lines = [line.rstrip('\n') for line in open(file_result)]
 
             for line in lines:
                 if line.startswith("File: "):
-                    found_misuse = line.split(' ', 1)[1]
+                    found_misuse = line[len("File: "):]
+                    found_misuse = normpath(found_misuse)
+                    # cut everything including the temp folder, then use [1:] to cut the leading slash
+                    found_misuse = found_misuse.split(settings.TEMP_SUBFOLDER, 1)[1][1:]
 
-                    if settings.TEMP_SUBFOLDER + '\\' in found_misuse:
-                        found_misuse = found_misuse.split(settings.TEMP_SUBFOLDER + '\\', 1)[1]
+                    print("Comparing found misuse {}".format(found_misuse))
 
-                    fix_filename = data_content["fix"]["files"][0]["name"]
-                    print(fix_filename)
-                    if 'trunk/' in fix_filename:
-                        fix_filename = fix_filename.split('trunk/', 1)[1]
-
-                    print("Comparing fixed file {} against file with misuse found {}".format(fix_filename, found_misuse))
-                    if normpath(found_misuse) == normpath(fix_filename):
+                    if found_misuse == fix_filename:
+                        print("Match found!")
                         return 1
-
+        print("No match")
         return 0
 
     dirs_results = [join(settings.RESULTS_PATH, result_dir) for result_dir in listdir(settings.RESULTS_PATH) if
