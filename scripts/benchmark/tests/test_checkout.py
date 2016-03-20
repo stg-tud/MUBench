e5b1d519a719744ee69908426725848c1f8176b5
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-03-20 15:30:53 +0100

    Checkout test now uses the new test environment

--- scripts/benchmark/tests/test_checkout.py
+++ scripts/benchmark/tests/test_checkout.py
@@ -1,7 +1,7 @@
 import unittest
 from genericpath import exists
 from os import mkdir
-from os.path import join
+from os.path import join, basename
 from shutil import rmtree
 from tempfile import mkdtemp
 
@@ -9,28 +9,28 @@ import utils.io
 
 import checkout
 import settings
+from tests.test_utils.test_env_util import TestEnvironment
 
 GIT = 'git'
 SVN = 'svn'
 SYNTHETIC = 'synthetic'
 
-GIT_REPOSITORY = 'https://github.com/stg-tud/MUBench.git'
-GIT_REVISION = '472c7b704bed5162e7ed3f0e9a7c464f448e7a83'
-GIT_PARENT_REVISION = 'b4db2acace11e41abdb80f3a902bf09148c8c831'
+GIT_REVISION = '2c4e93c712461ae20051409f472e4857e2189393'
+GIT_PARENT_REVISION = '451c2fd61437c1a4616f08cc1849feacc0be4839'
 
-SVN_REPOSITORY = 'svn://svn.code.sf.net/p/itext/code/trunk'  # TODO set these values and remove skip from svn test(s)
+SVN_REPOSITORY = 'svn://svn.code.sf.net/p/itext/code/trunk'  # TODO setup local svn repository in test env instead
 SVN_REVISION = 5091
 SVN_PARENT_REVISION = SVN_REVISION - 1
-
-SYNTHETIC_REPOSITORY = 'synthetic-cme.java'
 
 
 class CheckoutTest(unittest.TestCase):
     def setUp(self):
+        self.test_env = TestEnvironment()
+        self.test_env.setup()
         self.temp_dir = mkdtemp()
 
     def test_creates_git_repository(self):
-        checkout.checkout_parent(GIT, GIT_REPOSITORY, GIT_REVISION, self.temp_dir, False)
+        checkout.checkout_parent(GIT, self.test_env.REPOSITORY_GIT, GIT_REVISION, self.temp_dir, False)
         repository = join(self.temp_dir, '.git')
         self.assertTrue(exists(repository))
 
@@ -40,32 +40,33 @@ class CheckoutTest(unittest.TestCase):
         self.assertTrue(exists(repository))
 
     def test_copies_synthetic_repository(self):
-        checkout.checkout_parent(SYNTHETIC, SYNTHETIC_REPOSITORY, None, self.temp_dir, False)
-        repository = join(self.temp_dir, SYNTHETIC_REPOSITORY)
+        checkout.checkout_parent(SYNTHETIC, self.test_env.REPOSITORY_SYNTHETIC, '', self.temp_dir, False)
+        repository = join(self.temp_dir, basename(self.test_env.REPOSITORY_SYNTHETIC))
         self.assertTrue(exists(repository))
 
     def test_checkout_fails_for_non_empty_target_dir(self):
         mkdir(join(self.temp_dir, 'something'))
         with self.assertRaises(ValueError):
-            checkout.checkout_parent(GIT, GIT_REPOSITORY, GIT_REVISION, self.temp_dir, False)
+            checkout.checkout_parent(GIT, self.test_env.REPOSITORY_GIT, GIT_REVISION, self.temp_dir, False)
 
     def test_checkout_fails_for_file_as_target_dir(self):
         file = join(self.temp_dir, 'file')
         utils.io.create_file_path(file)
         with self.assertRaises(ValueError):
-            checkout.checkout_parent(GIT, GIT_REPOSITORY, GIT_REVISION, file, False)
+            checkout.checkout_parent(GIT, self.test_env.REPOSITORY_GIT, GIT_REVISION, file, False)
 
     def test_checkout_fails_for_unknown_vcs(self):
         with self.assertRaises(ValueError):
-            checkout.checkout_parent('invalid vcs', GIT_REPOSITORY, GIT_REVISION, self.temp_dir, False)
+            checkout.checkout_parent('invalid vcs', self.test_env.REPOSITORY_GIT, GIT_REVISION, self.temp_dir, False)
 
     def test_logs_into_log_folder(self):
         settings.LOG_PATH = join(self.temp_dir, 'logs')
         settings.LOG_FILE_CHECKOUT = join(settings.LOG_PATH, 'checkout.log')
-        checkout.checkout_parent(GIT, GIT_REPOSITORY, GIT_REVISION, self.temp_dir, False)
+        checkout.checkout_parent(GIT, self.test_env.REPOSITORY_GIT, GIT_REVISION, self.temp_dir, False)
         self.assertTrue(exists(settings.LOG_FILE_CHECKOUT))
 
     def tearDown(self):
+        self.test_env.teardown()
         rmtree(self.temp_dir, ignore_errors=True)
 
 
