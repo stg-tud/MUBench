320974dbfb5fde3ab1c24fa7f6b2691b08c4aee1
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-03-20 11:38:34 +0100

    settings doesn't need to initialize directories anymore since io util takes care of this now

--- scripts/benchmark/settings.py
+++ scripts/benchmark/settings.py
@@ -26,7 +26,3 @@ LOG_FILE_RESULTS_EVALUATION = join(LOG_PATH, "results-evaluation.log")
 TEMP_SUBFOLDER = "misuse-checkout"  # used as subfolder in the temp path (will show up in misuse detection results)
 VERBOSE = True  # prints more information to console if True
 IGNORES = []  # all data files which contain this substring will be ignored by the benchmark
-
-# INITIALIZE DIRECTORIES
-makedirs(RESULTS_PATH, exist_ok=True)
-makedirs(LOG_PATH, exist_ok=True)
