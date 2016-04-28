23636e27bfbbf293fc60261ca1dadd9fc5edde47
Mattis Manfred Kaemmerer <M8is@live.de>
2016-04-28 19:16:59 +0200

    Split command line parse tests into several test case classes (one for each mode)

--- benchmark/tests/utils/test_command_line_util.py
+++ benchmark/tests/utils/test_command_line_util.py
@@ -3,7 +3,16 @@ import unittest
 from utils import command_line_util
 
 
-class CommandLineUtilTest(unittest.TestCase):
+class InvalidModeTest(unittest.TestCase):
+    def setUp(self):
+        self.parser = command_line_util.get_command_line_parser()
+
+    def test_stops_on_invalid_mode(self):
+        with self.assertRaises(SystemExit):
+            self.parser.parse_args(['invalid'])
+
+
+class CheckModeTest(unittest.TestCase):
     def setUp(self):
         self.parser = command_line_util.get_command_line_parser()
 
@@ -11,31 +20,43 @@ class CommandLineUtilTest(unittest.TestCase):
         result = self.parser.parse_args(['check'])
         self.assertEqual('check', result.mode)
 
+
+class CheckoutModeTest(unittest.TestCase):
+    def setUp(self):
+        self.parser = command_line_util.get_command_line_parser()
+
     def test_mode_checkout(self):
         result = self.parser.parse_args(['checkout'])
         self.assertEqual('checkout', result.mode)
 
-    def test_mode_mine_fails_without_miner(self):
+
+class MineModeTest(unittest.TestCase):
+    def setUp(self):
+        self.parser = command_line_util.get_command_line_parser()
+
+    def test_fails_without_miner(self):
         with self.assertRaises(SystemExit):
             self.parser.parse_args(['mine'])
 
-    def test_mode_mine_with_miner(self):
+    def test_with_miner(self):
         test_miner = 'dummy-miner'
         result = self.parser.parse_args(['mine', test_miner])
         self.assertEqual(test_miner, result.miner)
 
-    def test_mode_evaluate_fails_without_detector(self):
+
+class EvalModeTest(unittest.TestCase):
+    def setUp(self):
+        self.parser = command_line_util.get_command_line_parser()
+
+    def test_fails_without_detector(self):
         with self.assertRaises(SystemExit):
             self.parser.parse_args(['eval'])
 
-    def test_mode_evaluate_with_detector(self):
+    def test_with_detector(self):
         test_detector = 'dummy-detector'
         result = self.parser.parse_args(['eval', test_detector])
         self.assertEqual(test_detector, result.detector)
 
-    def test_stops_on_invalid_mode(self):
-        with self.assertRaises(SystemExit):
-            self.parser.parse_args(['invalid'])
 
 if __name__ == '__main__':
     unittest.main()
