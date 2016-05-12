3a2ccb0041e93d81f406c7bf593cbad1431e5696
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-05-12 13:31:34 +0200

    Refactored result_evaluation module

--- benchmark/result_evaluation.py
+++ benchmark/result_evaluation.py
@@ -1,17 +1,14 @@
-import sys
-import traceback
 from genericpath import isdir, exists, isfile, getsize
 from os import listdir
 from os.path import join, splitext
 from os.path import normpath, basename
-
-from typing import Dict, Tuple, Optional, Union
+from typing import Dict, Tuple, Optional, Union, List, Any
 
 from benchmark import datareader
 from benchmark.detector_runner import DetectorRunner
 from benchmark.utils.data_util import normalize_result_misuse_path, normalize_data_misuse_path
 from benchmark.utils.dotgraph_util import get_labels_from_result_file, get_labels_from_data_content
-from benchmark.utils.io import safe_open, safe_write
+from benchmark.utils.io import safe_open
 from benchmark.utils.printing import subprocess_print, subprocess_print_append
 
 
@@ -34,9 +31,8 @@ class ResultEvaluation:
             detector_runner = DetectorRunner(data_path, detector, checkout_base_dir, results_path, None, [""], [])
             detector_runner.run_detector_on_all_data()
 
-    def evaluate_single_result(self,
-                               data_file: str,
-                               data_content: Dict[str, Union[Dict, str]]) -> Tuple[str, Optional[int]]:
+    def evaluate_single_result(self, data_file: str, data_content: Dict[str, Any]) -> Tuple[str, Optional[int]]:
+
         dirs_results = [join(self.results_path, result_dir) for result_dir in listdir(self.results_path) if
                         isdir(join(self.results_path, result_dir)) and not result_dir == '_LOGS']
 
@@ -70,80 +66,67 @@ class ResultEvaluation:
                     else:
                         subprocess_print_append("no hit")
                         return basename(data_file), 0
+
         subprocess_print_append("ignored (no available findings)")
         return basename(data_file), None
 
-    def evaluate_results(self) -> Tuple[int, int, int]:
-        try:
-            results = datareader.on_all_data_do(self.data_path, self.evaluate_single_result,
-                                                [""], [])  # this modules handles white and blacklisting separately
+    def evaluate_results(self) -> None:
+        results = datareader.on_all_data_do(self.data_path, self.evaluate_single_result, [""], [])
+        self.output_results(results)
 
-            def to_data_name(result):
-                return result[0]
+    def output_results(self, results: List[Tuple[str, int]]) -> None:
+        def to_data_name(result: Tuple[str, int]) -> str:
+            return result[0]
 
-            def to_success(result):
-                return result[1]
+        def to_success(result: Tuple[str, int]) -> int:
+            return result[1]
 
-            applied_results = [result for result in results if result[1] is not None]
+        applied_results = [result for result in results if result[1] is not None]
+        total = len(results)
+        applied = len(applied_results)
+        found = sum(map(to_success, applied_results))
 
-            total = len(results)
-            applied = len(applied_results)
-            found = sum(map(to_success, applied_results))
+        def was_successful(result: Tuple[str, int]) -> bool:
+            return result[1] is 1
 
-            def was_successful(result):
-                return result[1] is 1
+        def was_not_successful(result: Tuple[str, int]) -> bool:
+            return result[1] is 0
 
-            def was_not_successful(result):
-                return result[1] is 0
+        def finished_with_error(result: Tuple[str, int]) -> bool:
+            return result[1] is None
 
-            def finished_with_error(result):
-                return result[1] is None
-
-            found_misuses = map(to_data_name, filter(was_successful, results))
-            not_found_misuses = map(to_data_name, filter(was_not_successful, results))
-            misuses_with_errors = map(to_data_name, filter(finished_with_error, results))
-
-            with safe_open(join(self.results_path, "Result.txt"), 'w+') as file_result:
-                print('----------------------------------------------', file=file_result)
-                print('Total number of misuses in the benchmark: ' + str(total), file=file_result)
-                print('Number of analyzed misuses (might be less due to ignore or errors): ' + str(applied),
-                      file=file_result)
-                print('Number of misuses found: ' + str(found), file=file_result)
-                print('----------------------------------------------', file=file_result)
-                print('These misuses were found:', file=file_result)
-                print('\n'.join(found_misuses), file=file_result)
-                print('----------------------------------------------', file=file_result)
-                print('These misuses were not found:', file=file_result)
-                print('\n'.join(not_found_misuses), file=file_result)
-                print('----------------------------------------------', file=file_result)
-                print('These cases encountered an error (see logs for more information) or were ignored:',
-                      file=file_result)
-                print('\n'.join(misuses_with_errors), file=file_result)
-                print('----------------------------------------------', file=file_result)
-
-            return total, applied, found
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            if not self.catch_errors:
-                raise
-
-            exception_string = traceback.format_exc()
-            print(exception_string)
-            safe_write(exception_string, join(self.results_path, '_LOGS', 'error.log'), append=True)
-            return -1, -1, -1
+        found_misuses = map(to_data_name, filter(was_successful, results))
+        not_found_misuses = map(to_data_name, filter(was_not_successful, results))
+        misuses_with_errors = map(to_data_name, filter(finished_with_error, results))
+        with safe_open(join(self.results_path, "Result.txt"), 'w+') as file_result:
+            print('----------------------------------------------', file=file_result)
+            print('Total number of misuses in the benchmark: ' + str(total), file=file_result)
+            print('Number of analyzed misuses (might be less due to ignore or errors): ' + str(applied),
+                  file=file_result)
+            print('Number of misuses found: ' + str(found), file=file_result)
+            print('----------------------------------------------', file=file_result)
+            print('These misuses were found:', file=file_result)
+            print('\n'.join(found_misuses), file=file_result)
+            print('----------------------------------------------', file=file_result)
+            print('These misuses were not found:', file=file_result)
+            print('\n'.join(not_found_misuses), file=file_result)
+            print('----------------------------------------------', file=file_result)
+            print('These cases encountered an error (see logs for more information) or were ignored:',
+                  file=file_result)
+            print('\n'.join(misuses_with_errors), file=file_result)
+            print('----------------------------------------------', file=file_result)
 
     @staticmethod
     def __is_file_found(result_file: str,
-                        data_content: Dict[str, Union[str, Dict]],
+                        data_content: Dict[str, Any],
                         checkout_base_dir: str,
                         log_stream) -> bool:
         lines = [line.rstrip('\n') for line in safe_open(result_file, 'r')]
 
         for line in lines:
-            if line.startswith("File: "):
-                # cut File: from the line to get the path
-                found_misuse = line[len("File: "):]
+            if line.startswith("file: "):
+                # cut file: from the line to get the path
+                found_misuse = line[len("file: "):]
                 normed_found_misuse = normalize_result_misuse_path(found_misuse, checkout_base_dir)
 
                 for misuse_file in data_content["fix"]["files"]:
@@ -162,7 +145,7 @@ class ResultEvaluation:
 
     @staticmethod
     def __is_label_found(result_file: str,
-                         data_content: Dict[str, Union[str, Dict]],
+                         data_content: Dict[str, Any],
                          log_stream) -> bool:
         misuse_labels = get_labels_from_data_content(data_content)
         result_labels = get_labels_from_result_file(result_file)
