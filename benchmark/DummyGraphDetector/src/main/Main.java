a23847edf0d95f07bc12b784e9bbd3a86d262199
Mattis Manfred Kaemmerer <M8is@live.de>
2016-05-23 12:38:28 +0200

    Added dummy-graph-detector which always outputs a specific misuse graph from our data

---
+++ benchmark/DummyGraphDetector/src/main/Main.java
@@ -0,0 +1,25 @@
+package main;
+
+import java.io.File;
+import java.io.PrintStream;
+
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) throws Exception {
+		String dirPath = args[0];
+		String resultFilePath = args[1];
+		System.out.println("Running on: " + dirPath);
+		System.out.println("Result  in: " + resultFilePath);
+
+		File resultFile = new File(resultFilePath);
+		try (PrintStream writer = new PrintStream(resultFile)) {
+			writer.println("digraph {");
+			writer.println("0 [label=\"StrBuilder#this#getNullText\"]");
+			writer.println("1 [label=\"String#str#length\"]");
+			writer.println("0 -> 1");
+			writer.println("}");
+		}
+	}
+}
