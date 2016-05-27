37d1ecd823c84c994c8991056cdaceef4bae8e33
Mattis Manfred Kaemmerer <M8is@live.de>
2016-05-27 19:00:05 +0200

    Compile now copies additional compile sources into the project folder before building

--- benchmark/compile.py
+++ benchmark/compile.py
@@ -1,14 +1,22 @@
 import subprocess
+from os.path import join, exists
+
+from shutil import copytree
 
 from benchmark.misuse import Misuse
 
 
 class Compile:
-    def __init__(self, outlog: str, errlog: str):
+    def __init__(self, checkout_base_dir: str, outlog: str, errlog: str):
+        self.checkout_base_dir = checkout_base_dir
         self.outlog = outlog
         self.errlog = errlog
 
     def build(self, misuse: Misuse):
+        additional_sources = misuse.additional_compile_sources
+        if exists(additional_sources):
+            copytree(additional_sources, join(self.checkout_base_dir, misuse.project_name))
+
         build_config = misuse.build_config
 
         if build_config is None:
--- benchmark/nosetests/test_compile.py
+++ benchmark/nosetests/test_compile.py
@@ -1,4 +1,4 @@
-from os.path import join
+from os.path import join, exists
 from shutil import rmtree
 from tempfile import mkdtemp
 
@@ -8,10 +8,14 @@ from benchmark.compile import Compile
 from benchmark.nosetests.test_misuse import TMisuse
 
 
+from benchmark.utils.io import create_file
+
+
 # noinspection PyAttributeOutsideInit
 class TestCompile:
     def setup(self):
         self.temp_dir = mkdtemp(prefix="mubench-compile-test_")
+        self.test_checkout_dir = join(self.temp_dir, "checkouts")
         self.outlog = join(self.temp_dir, "out.log")
         self.errlog = join(self.temp_dir, "err.log")
 
@@ -21,15 +25,21 @@ class TestCompile:
     def test_runs_commands(self):
         called_commands = []
 
-        def _call_mock(self: Compile, command: str): called_commands.append(command)
+        def _call_mock(command: str): called_commands.append(command)
 
-        uut = Compile(outlog=self.outlog, errlog=self.errlog)
+        uut = Compile(checkout_base_dir=self.test_checkout_dir, outlog=self.outlog, errlog=self.errlog)
         uut._call = _call_mock
+
+        misuse = TMisuse(self.temp_dir, {"build": {"src": "", "commands": ["c1", "c2", "c3"], "classes": ""}})
+
+        uut.build(misuse)
+
+        assert_equals(["c1", "c2", "c3"], called_commands)
 
     def test_uses_out_log(self):
         print_to_out_log = "echo Hallo Echo!"
-        uut = Compile(outlog=self.outlog, errlog=self.errlog)
-        misuse = TMisuse("", {"build": {"src": "", "commands": [print_to_out_log], "classes": ""}})
+        uut = Compile(checkout_base_dir=self.test_checkout_dir, outlog=self.outlog, errlog=self.errlog)
+        misuse = TMisuse(self.temp_dir, {"build": {"src": "", "commands": [print_to_out_log], "classes": ""}})
 
         uut.build(misuse)
 
@@ -39,8 +49,8 @@ class TestCompile:
 
     def test_uses_err_log(self):
         print_to_err_log = ">&2 echo Hallo Echo!"
-        uut = Compile(outlog=self.outlog, errlog=self.errlog)
-        misuse = TMisuse("", {"build": {"src": "", "commands": [print_to_err_log], "classes": ""}})
+        uut = Compile(checkout_base_dir=self.test_checkout_dir, outlog=self.outlog, errlog=self.errlog)
+        misuse = TMisuse(self.temp_dir, {"build": {"src": "", "commands": [print_to_err_log], "classes": ""}})
 
         uut.build(misuse)
 
@@ -49,6 +59,20 @@ class TestCompile:
             assert_equals("Hallo Echo!\n", content)
 
     def test_no_fail_without_build_config(self):
-        uut = Compile(outlog=self.outlog, errlog=self.errlog)
-        misuse = TMisuse()
+        uut = Compile(checkout_base_dir=self.test_checkout_dir, outlog=self.outlog, errlog=self.errlog)
+        misuse = TMisuse(join(self.temp_dir, "project"))
         uut.build(misuse)
+
+    def test_copies_over_build_files(self):
+        test_sources = join(self.temp_dir, "project", "compile")
+        create_file(join(test_sources, "file1.java"))
+        create_file(join(test_sources, "src", "file2.java"))
+
+        uut = Compile(checkout_base_dir=self.test_checkout_dir, outlog=self.outlog, errlog=self.errlog)
+
+        misuse = TMisuse(join(self.temp_dir, "project"))
+
+        uut.build(misuse)
+
+        assert exists(join(self.test_checkout_dir, "project", "file1.java"))
+        assert exists(join(self.test_checkout_dir, "project", "src", "file2.java"))
