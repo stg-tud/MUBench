fb9af5cbef507c4ad932f07f8b3483d8679e47fc
Mattis Manfred Kaemmerer <M8is@live.de>
2016-05-27 18:14:08 +0200

    Implemented Compile step to build projects

---
+++ benchmark/compile.py
@@ -0,0 +1,19 @@
+import subprocess
+
+from benchmark.misuse import Misuse
+
+
+class Compile:
+    def __init__(self, outlog: str, errlog: str):
+        self.outlog = outlog
+        self.errlog = errlog
+
+    def build(self, misuse: Misuse):
+        build_config = misuse.build_config
+
+        for command in build_config.commands:
+            self._call(command)
+
+    def _call(self, command: str):
+        with open(self.outlog, 'a+') as outlog, open(self.errlog, 'a+') as errlog:
+            subprocess.call(command, shell=True, stdout=outlog, stderr=errlog, bufsize=1)
---
+++ benchmark/nosetests/test_compile.py
@@ -0,0 +1,52 @@
+from os.path import join
+from shutil import rmtree
+from tempfile import mkdtemp
+
+from nose.tools import assert_equals
+
+from benchmark.compile import Compile
+from benchmark.nosetests.test_misuse import TMisuse
+
+
+# noinspection PyAttributeOutsideInit
+class TestCompile:
+    def setup(self):
+        self.temp_dir = mkdtemp(prefix="mubench-compile-test_")
+        self.outlog = join(self.temp_dir, "out.log")
+        self.errlog = join(self.temp_dir, "err.log")
+
+    def teardown(self):
+        rmtree(self.temp_dir, ignore_errors=True)
+
+    def test_runs_commands(self):
+        called_commands = []
+
+        def _call_mock(self: Compile, command: str): called_commands.append(command)
+
+        uut = Compile(outlog=self.outlog, errlog=self.errlog)
+        uut._call = _call_mock
+
+    def test_uses_out_log(self):
+        print_to_out_log = "echo Hallo Echo!"
+        uut = Compile(outlog=self.outlog, errlog=self.errlog)
+        misuse = TMisuse("", {"build": {"src": "", "commands": [print_to_out_log], "classes": ""}})
+
+        uut.build(misuse)
+
+        with open(self.outlog, 'r') as actual_log:
+            content = actual_log.read()
+            assert_equals("Hallo Echo!\n", content)
+
+    def test_uses_err_log(self):
+        print_to_err_log = ">&2 echo Hallo Echo!"
+        uut = Compile(outlog=self.outlog, errlog=self.errlog)
+        misuse = TMisuse("", {"build": {"src": "", "commands": [print_to_err_log], "classes": ""}})
+
+        uut.build(misuse)
+
+        with open(self.errlog, 'r') as actual_log:
+            content = actual_log.read()
+            assert_equals("Hallo Echo!\n", content)
+
+    
+
