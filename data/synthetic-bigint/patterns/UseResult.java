296d8ef834bf870f9b77fbdf824ab5f19482deb2
Mattis Manfred Kaemmerer <M8is@live.de>
2016-06-02 21:07:50 +0200

    Fixed error in bigint pattern

--- data/synthetic-bigint/patterns/UseResult.java
+++ data/synthetic-bigint/patterns/UseResult.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 
 public class UseResult {	
-	public void pattern(String value, int bit) {
+	public BigInteger pattern(String value, int bit) {
 		BigInteger i = new BigInteger(value);
 		i = i.setBit(bit);
 		return i;
