842b54343d22a7fe2a7b941c088b30721b6cbadd
Mattis Manfred Kaemmerer <M8is@live.de>
2016-06-02 14:04:34 +0200

    Implemented Pattern class which handles pattern files and can duplicate the pattern file to a target location

---
+++ benchmark/nosetests/test_pattern.py
@@ -0,0 +1,79 @@
+from os import makedirs
+from os.path import join, exists
+from tempfile import mkdtemp
+
+from nose.tools import assert_raises
+from shutil import rmtree
+
+from benchmark.pattern import Pattern, NoPatternFileError
+from benchmark.utils.io import create_file
+
+
+# noinspection PyAttributeOutsideInit
+class TestPattern:
+    def setup(self):
+        self.temp_dir = mkdtemp(prefix="mubench-pattern-test_")
+        self.orig_dir = join(join(self.temp_dir, "origin"))
+        makedirs(self.orig_dir, exist_ok=True)
+
+        self.pattern_file_base_name = "pattern"
+        self.pattern_file_extension = ".java"
+        self.pattern_file_name = self.pattern_file_base_name + self.pattern_file_extension
+        self.pattern_file_path = join(self.orig_dir, self.pattern_file_name)
+        create_file(self.pattern_file_path)
+
+    def teardown(self):
+        rmtree(self.temp_dir, ignore_errors=True)
+
+    def test_fails_on_non_existent_file(self):
+        uut = Pattern(join(self.temp_dir, "does-not-exist.java"))
+        assert_raises(NoPatternFileError, uut.is_valid)
+
+    def test_fails_on_file_with_bad_extension(self):
+        existing_file_with_bad_extension = join(self.temp_dir, "file.txt")
+        create_file(existing_file_with_bad_extension)
+        uut = Pattern(existing_file_with_bad_extension)
+        assert_raises(NoPatternFileError, uut.is_valid)
+
+    def test_copy_file(self):
+        destination = join(self.temp_dir, "destination")
+        uut = Pattern(self.pattern_file_path)
+        uut.copy(destination)
+        assert exists(join(destination, self.pattern_file_base_name + "0" + self.pattern_file_extension))
+
+    def test_copy_file_twice(self):
+        destination = join(self.temp_dir, "destination")
+        uut = Pattern(self.pattern_file_path)
+        uut.copy(destination)
+        uut.copy(destination)
+        assert exists(join(destination, self.pattern_file_base_name + "1" + self.pattern_file_extension))
+
+    def test_copy_file_twice_changes_class_name(self):
+        destination = join(self.temp_dir, "destination")
+
+        test_pattern_file = join(self.orig_dir, "UseResult.java")
+        test_content = ["import java.math.BigInteger;", "", "public class UseResult {",
+                        "\tpublic void pattern(String value, int bit) {",
+                        "\t\tBigInteger i = new BigInteger(value);",
+                        "\t\ti = i.setBit(bit);",
+                        "\t\treturn i;",
+                        "\t}",
+                        "}"]
+        with open(test_pattern_file, 'w') as second_copy:
+            second_copy.writelines(test_content)
+
+        uut = Pattern(test_pattern_file)
+
+        uut.copy(destination)
+        uut.copy(destination)
+
+        with open(join(destination, "UseResult1.java"), "r") as second_copy:
+            actual_content = second_copy.read()
+
+        assert "class UseResult1" in actual_content
+
+    def test_duplicate(self):
+        destination = join(self.temp_dir, "destination")
+        uut = Pattern(self.pattern_file_path)
+        uut.duplicate(destination, 5)
+        assert exists(join(destination, self.pattern_file_base_name + "4" + self.pattern_file_extension))
---
+++ benchmark/pattern.py
@@ -0,0 +1,62 @@
+from distutils.file_util import copy_file
+from fileinput import FileInput
+from os import makedirs
+from os.path import exists, basename, join, splitext, dirname
+
+
+class Pattern:
+    def __init__(self, path: str):
+        self.path = path
+
+    def is_valid(self) -> bool:
+        if not self.path.endswith(".java") or not exists(self.path):
+            raise NoPatternFileError("Pattern file is not valid!")
+
+    @property
+    def orig_dir(self):
+        return dirname(self.path)
+
+    @property
+    def file_name(self):
+        return splitext(basename(self.path))[0]
+
+    @property
+    def class_name(self):
+        return self.file_name
+
+    @property
+    def file_extension(self):
+        return splitext(self.path)[1]
+
+    def duplicate(self, destination: str, times: int) -> str:
+        for i in range(times):
+            self.copy(destination)
+        return destination
+
+    def copy(self, destination: str) -> str:
+        makedirs(destination, exist_ok=True)
+
+        i = 0
+        while exists(self._get_destination_file(destination, i)):
+            i += 1
+
+        copied_file = self._get_destination_file(destination, i)
+        copy_file(self.path, copied_file)
+
+        self._replace_class_name(copied_file, i)
+
+        return destination
+
+    def _get_destination_file(self, destination: str, i: int) -> str:
+        return join(destination, self.file_name + str(i) + self.file_extension)
+
+    def _replace_class_name(self, copied_file: str, i: int) -> None:
+        with FileInput(copied_file, inplace=True) as file:
+            for line in file:
+                if self.class_name in line:
+                    # FileInput inplace redirects stdout to the file
+                    print(line.replace(self.class_name, self.class_name + str(i)), end='')
+
+
+class NoPatternFileError(FileNotFoundError):
+    pass
