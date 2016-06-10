f793e166afb705caad99277de362e2e57443c5c2
Mattis Manfred Kaemmerer <M8is@live.de>
2016-06-10 12:44:12 +0200

    Implemented md5 check in web_util

--- benchmark/utils/web_util.py
+++ benchmark/utils/web_util.py
@@ -5,17 +5,25 @@ from os.path import exists
 
 from typing import Optional
 
-from benchmark.utils.io import create_file_path
-
 
 def download(url: str, file: str, md5_file: Optional[str] = None) -> bool:
-    create_file_path(file)
-
     with urllib.request.urlopen(url) as response, open(file, 'wb') as out_file:
         shutil.copyfileobj(response, out_file)
 
     if not exists(file):
         return False
+
+    if md5_file is not None:
+        return _check_md5(file, md5_file)
+
+    return True
+
+
+def _check_md5(file, reference_file):
+    actual_md5 = _md5(file)
+    with open(reference_file, 'r') as reference_md5_file:
+        reference_md5 = reference_md5_file.read().rstrip("\n")
+    return reference_md5 == actual_md5
 
 
 # source: http://stackoverflow.com/a/3431838
