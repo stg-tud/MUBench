4aa832d9e54e0de6f0baf428f751ee2899888c7b
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-06-21 18:59:33 +0200

    Tables can now be read back into a dictionary from a .csv file

--- benchmark/utils/csv_util.py
+++ benchmark/utils/csv_util.py
@@ -11,3 +11,16 @@ def write_table(file: str, headers: List[str], content: Dict[str, Dict[str, str]
         for row_key in sorted(content):
             print(row_key, end='', file=result_file)
             w.writerow({column_key: content[row_key].get(column_key) or '' for column_key in headers[1:]})
+
+
+def read_table(file: str, row_key: str) -> Dict[str, Dict[str, str]]:
+    result = dict()
+
+    with open(file, 'r') as source:
+        r = csv.DictReader(source)
+        for row in r:
+            key = row.pop(row_key)
+            row = {k: v for k, v in row.items() if v}  # remove all empty values
+            result[key] = row
+
+    return result
--- benchmark_tests/utils/test_csv_util.py
+++ benchmark_tests/utils/test_csv_util.py
@@ -36,3 +36,18 @@ class TestCsvUtil:
             actual = actual_file.read()
 
         assert_equals(expected, actual)
+
+    def test_read_table(self):
+        # Detector  MU1 MU2 MU3
+        # D1        0   1   0
+        # D2        1       0
+        # D3        1   0   1
+
+        test_content = {"D1": {"MU1": "0", "MU2": "1", "MU3": "0"},
+                        "D2": {"MU1": "1", "MU3": "0"},
+                        "D3": {"MU1": "1", "MU2": "0", "MU3": "1"}}
+
+        csv_util.write_table(self.temp_file, ["Detector", "MU1", "MU2", "MU3"], test_content)
+        read_content = csv_util.read_table(self.temp_file, "Detector")
+
+        assert_equals(test_content, read_content)
