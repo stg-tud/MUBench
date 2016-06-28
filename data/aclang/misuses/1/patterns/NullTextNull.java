0c37c24c6d3dbf3fe064113a8c42ec98a60e9ae1
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-06-28 13:30:42 +0200

    Fix pattern.

--- data/aclang/misuses/1/patterns/NullTextNull.java
+++ data/aclang/misuses/1/patterns/NullTextNull.java
@@ -2,7 +2,6 @@ import org.apache.commons.lang.text.StrBuilder;
 
 class NullTextNull extends StrBuilder {
   String pattern(Object obj) {
-    this.ensureCapacity(size + width);
     String str = (obj == null ? this.getNullText() : obj.toString());
     if (str == null) {
       str = "";
