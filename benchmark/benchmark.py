9a9dae1b8b3d2d904b6e6e0046f05cb158cc853b
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-07-03 11:05:58 +0200

    Cleanup benchmark

--- benchmark/benchmark.py
+++ benchmark/benchmark.py
@@ -4,7 +4,6 @@ import logging.handlers
 import sys
 from os import listdir
 from os.path import join, realpath, isdir, exists
-from typing import Optional, List
 
 from benchmark.subprocesses.check import check_prerequisites
 from benchmark.subprocesses.result_processing.visualize_results import Visualizer
@@ -24,89 +23,79 @@ class Benchmark:
     CHECKOUTS_PATH = realpath("checkouts")
     RESULTS_PATH = realpath("results")
 
-    def __init__(self,
-                 detector: str,
-                 timeout: Optional[int],
-                 black_list: List[str],
-                 white_list: List[str],
-                 java_options: List[str],
-                 force_detect: bool,
-                 force_checkout: bool,
-                 force_compile: bool,
-                 stats_script: str):
-        # command-line options
-        self.detector = detector
-        self.timeout = timeout
-        self.black_list = black_list
-        self.white_list = white_list
-        self.java_options = java_options
-        self.force_checkout = force_checkout
-        self.force_detect = force_detect
-        self.force_compile = force_compile
-        self.pattern_frequency = 20  # TODO make configurable
-        self.stats_script = stats_script
-
-        self.results_path = join(Benchmark.RESULTS_PATH, self.detector)
-
+    def __init__(self, config):
         self.detector_result_file = 'findings.yml'
         self.eval_result_file = 'result.csv'
         self.reviewed_eval_result_file = 'reviewed-result.csv'
         self.visualize_result_file = 'result.csv'
 
-        self.runner = TaskRunner(Benchmark.DATA_PATH, self.white_list, self.black_list)
+        self.config = config
 
-    def run_checkout(self) -> None:
-        self._setup_checkout()
-        self.run()
+        if 'white_list' not in config:
+            config.white_list = []
+        if 'black_list' not in config:
+            config.black_list = []
+        self.runner = TaskRunner(Benchmark.DATA_PATH, config.white_list, config.black_list)
 
-    def run_compile(self) -> None:
-        self._setup_checkout()
-        self._setup_compile()
-        self.run()
-
-    def run_detect(self) -> None:
-        self._setup_checkout()
-        self._setup_compile()
-        self._setup_detect()
-        self.run()
-
-    def run_evaluate(self) -> None:
-        self._setup_checkout()
-        self._setup_compile()
-        self._setup_detect()
-        self._setup_eval()
-        self.run()
-
-    def run_visualize(self) -> None:
+    def _run_visualize(self) -> None:
         visualizer = Visualizer(Benchmark.RESULTS_PATH, self.reviewed_eval_result_file, self.visualize_result_file,
                                 Benchmark.DATA_PATH)
         visualizer.create()
 
-    def run_stats(self) -> None:
-        stats_calculator = stats.get_calculator(self.stats_script)
+    def _setup_stats(self) -> None:
+        stats_calculator = stats.get_calculator(self.config.script)
         self.runner.add(stats_calculator)
         self.run()
 
     def _setup_checkout(self):
-        checkout_handler = Checkout(Benchmark.CHECKOUTS_PATH, self.force_checkout)
+        checkout_handler = Checkout(Benchmark.CHECKOUTS_PATH, self.config.force_checkout)
         self.runner.add(checkout_handler)
 
     def _setup_compile(self):
-        compile_handler = Compile(Benchmark.CHECKOUTS_PATH, Benchmark.CHECKOUTS_PATH, self.pattern_frequency,
-                                  self.force_compile)
+        self._setup_checkout()
+
+        compile_handler = Compile(Benchmark.CHECKOUTS_PATH, Benchmark.CHECKOUTS_PATH, self.config.pattern_frequency,
+                                  self.config.force_compile)
         self.runner.add(compile_handler)
 
     def _setup_detect(self):
-        detector_runner = Detect(self.detector, self.detector_result_file, Benchmark.CHECKOUTS_PATH, self.results_path,
-                                 self.timeout, self.java_options, self.force_detect)
+        self._setup_checkout()
+        self._setup_compile()
+
+        results_path = join(Benchmark.RESULTS_PATH, self.config.detector)
+        detector_runner = Detect(self.config.detector, self.detector_result_file, Benchmark.CHECKOUTS_PATH,
+                                 results_path,
+                                 self.config.timeout, self.config.java_options, self.config.force_detect)
         self.runner.add(detector_runner)
 
     def _setup_eval(self):
-        evaluation_handler = Evaluate(self.results_path, self.detector_result_file, Benchmark.CHECKOUTS_PATH,
+        self._setup_checkout()
+        self._setup_compile()
+        self._setup_detect()
+
+        results_path = join(Benchmark.RESULTS_PATH, self.config.detector)
+        evaluation_handler = Evaluate(results_path, self.detector_result_file, Benchmark.CHECKOUTS_PATH,
                                       self.eval_result_file)
         self.runner.add(evaluation_handler)
 
     def run(self) -> None:
+        check_prerequisites()
+        if config.subprocess == 'visualize':
+            self._run_visualize()
+            return
+        elif config.subprocess == 'check':
+            pass
+        elif config.subprocess == 'checkout':
+            self._setup_checkout()
+        elif config.subprocess == 'compile':
+            self._setup_compile()
+        elif config.subprocess == 'detect':
+            self._setup_detect()
+        elif config.subprocess == 'eval':
+            self._setup_eval()
+        elif config.subprocess == 'stats':
+            self._setup_stats()
+
         self.runner.run()
 
 
@@ -131,26 +120,6 @@ available_detectors = [detector for detector in listdir(detectors_path) if isdir
 available_scripts = stats.get_available_calculator_names()
 config = command_line_util.parse_args(sys.argv, available_detectors, available_scripts)
 
-if 'detector' not in config:
-    config.detector = ''
-if 'white_list' not in config:
-    config.white_list = []
-if 'black_list' not in config:
-    config.black_list = []
-if 'timeout' not in config:
-    config.timeout = None
-if 'java_options' not in config:
-    config.java_options = []
-if 'force_detect' not in config:
-    config.force_detect = False
-if 'force_checkout' not in config:
-    config.force_checkout = False
-if 'force_compile' not in config:
-    config.force_compile = False
-if 'script' not in config:
-    config.script = ''
-
-
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 handler = logging.StreamHandler()
@@ -172,23 +141,5 @@ logger.debug("- Configuration       :")
 for key in config.__dict__:
     logger.debug("    - %s = %r", key.ljust(15), config.__dict__[key])
 
-benchmark = Benchmark(detector=config.detector, white_list=config.white_list, black_list=config.black_list,
-                      timeout=config.timeout, java_options=config.java_options, force_detect=config.force_detect,
-                      force_checkout=config.force_checkout, force_compile=config.force_compile, stats_script=config.script)
-
-check_prerequisites()
-
-if config.subprocess == 'check':
-    pass
-if config.subprocess == 'checkout':
-    benchmark.run_checkout()
-if config.subprocess == 'compile':
-    benchmark.run_compile()
-if config.subprocess == 'detect':
-    benchmark.run_detect()
-if config.subprocess == 'eval':
-    benchmark.run_evaluate()
-if config.subprocess == 'visualize':
-    benchmark.run_visualize()
-if config.subprocess == 'stats':
-    benchmark.run_stats()
+benchmark = Benchmark(config)
+benchmark.run()
