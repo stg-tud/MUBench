e7bf2957a365c06265b42e9395a765df43fc7173
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-08-07 00:39:12 +0200

    Check folder exists before listing it.

--- benchmark/subprocesses/tasks/implementations/review/review_utils.php
+++ benchmark/subprocesses/tasks/implementations/review/review_utils.php
@@ -2,9 +2,11 @@
 
 function get_reviewer_names($dir, $prefix) {
     $reviewer_names = array();
-    foreach (scandir($dir) as $file) {
-        if (strpos($file, $prefix) === 0 && substr($file, -4) == ".yml") {
-            $reviewer_names[] = basename(substr($file, strlen($prefix) + 1), ".yml");
+    if (is_dir($dir)) {
+        foreach (scandir($dir) as $file) {
+            if (strpos($file, $prefix) === 0 && substr($file, -4) == ".yml") {
+                $reviewer_names[] = basename(substr($file, strlen($prefix) + 1), ".yml");
+            }
         }
     }
     return $reviewer_names;
