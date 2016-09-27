d3765e537d431856adeee9bb1623456bf0359670
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-09-27 14:52:12 +0200

    Add potential-hit matching to Finding.

---
+++ benchmark/data/finding.py
@@ -0,0 +1,44 @@
+from typing import Dict
+
+from benchmark.data.misuse import Misuse
+
+
+class Finding(Dict[str, str]):
+    def __init__(self, data: Dict[str, str]):
+        super().__init__()
+        self.update(data)
+
+    def is_potential_hit(self, misuse: Misuse, method_name_only: bool=False):
+        return self.__is_match_by_file(misuse.location.file) and \
+               self.__is_match_by_method(misuse.location.method, method_name_only)
+
+    def __is_match_by_file(self, misuse_file: str):
+        finding_file = self.__file()
+
+        # If file is an inner class "Outer$Inner.class", the source file is "Outer.java".
+        if "$" in finding_file:
+            finding_file = finding_file.split("$", 1)[0] + ".java"
+
+        # If file is a class file "A.class", the source file is "A.java".
+        if finding_file.endswith(".class"):
+            finding_file = finding_file[:-5] + "java"
+
+        return finding_file.endswith(misuse_file)
+
+    def __file(self):
+        return self["file"]
+
+    def __is_match_by_method(self, misuse_method, method_name_only: bool=False):
+        finding_method = self.__method()
+
+        if method_name_only:
+            finding_method = finding_method.split("(")[0]
+
+        # If detector reports only method names, this ensures we don't match prefixes of method names
+        if "(" not in finding_method:
+            finding_method += "("
+
+        return finding_method in misuse_method
+
+    def __method(self):
+        return self["method"] if "method" in self else ""
---
+++ benchmark_tests/data/test_finding.py
@@ -0,0 +1,62 @@
+from typing import Dict
+
+from benchmark.data.finding import Finding
+from benchmark_tests.test_utils.data_util import create_misuse
+
+
+class TestPotentialHit:
+    # noinspection PyAttributeOutsideInit
+    def setup(self):
+        self.misuse = create_misuse('misuse', meta={"location": {"file": "a", "method": "m()"}})
+
+    def test_matches_on_file(self):
+        self.misuse.location.file = "some-class.java"
+        self.assert_potential_hit({"file": "some-class.java"})
+
+    def test_matches_on_file_absolute(self):
+        self.misuse.location.file = "java/main/some-class.java"
+        self.assert_potential_hit({"file": "/some/prefix/java/main/some-class.java"})
+
+    def test_matches_on_class(self):
+        self.misuse.location.file = "some-class.java"
+        self.assert_potential_hit({"file": "some-class.class"})
+
+    def test_matches_on_inner_class(self):
+        self.misuse.location.file = "some-class.java"
+        self.assert_potential_hit({"file": "some-class$inner-class.class"})
+
+    def test_differs_on_method(self):
+        self.misuse.location.method = "method()"
+        self.assert_no_potential_hit({"method": "other_method()"})
+
+    def test_matches_on_method_name(self):
+        self.misuse.location.method = "method(A, B)"
+        self.assert_potential_hit({"method": "method"})
+
+    def test_differs_on_method_name_prefix(self):
+        self.misuse.location.method = "appendX"
+        self.assert_no_potential_hit({"method": "append"})
+
+    def test_matches_on_method_signature(self):
+        self.misuse.location.method = "method(A, B)"
+        self.assert_potential_hit({"method": "method(A, B)"})
+
+    def test_matches_on_method_signature_name_only(self):
+        self.misuse.location.method = "method(A)"
+        self.assert_potential_hit({"method": "method(p.A)"}, True)
+
+    def assert_potential_hit(self, finding_data: Dict[str, str], method_name_only: bool=False):
+        finding = self.create_finding(finding_data)
+        assert finding.is_potential_hit(self.misuse, method_name_only)
+
+    def assert_no_potential_hit(self, finding_data: Dict[str, str]):
+        finding = self.create_finding(finding_data)
+        assert not finding.is_potential_hit(self.misuse)
+
+    def create_finding(self, finding_data: Dict[str, str]):
+        if "file" not in finding_data:
+            finding_data["file"] = self.misuse.location.file
+        if "method" not in finding_data:
+            finding_data["method"] = self.misuse.location.method
+
+        return Finding(finding_data)
