efb90f3407881f36c4961c938af0cf0f490c4419
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-09-27 12:29:32 +0200

    Remove duplication in tests.

--- benchmark_tests/data/test_experiment.py
+++ benchmark_tests/data/test_experiment.py
@@ -4,6 +4,7 @@ from nose.tools import assert_equals
 
 from benchmark.data.detector import Detector
 from benchmark.data.experiment import Experiment
+from benchmark.data.run import Run
 from benchmark_tests.test_utils.data_util import create_project, create_version, create_misuse
 
 
@@ -13,35 +14,33 @@ class TestExperiment:
 
         assert_equals(experiment.detector_mode, Experiment.RUN_MODES[Experiment.PROVIDED_PATTERNS])
 
-    def test_run_path(self):
-        detector = Detector("", "-detector-")
-        experiment = Experiment(Experiment.BENCHMARK, detector, "-findings_path-", "")
-        project = create_project("-project-")
-        version = create_version("-version-", project=project)
+    # noinspection PyAttributeOutsideInit
+    def setup(self):
+        self.detector = Detector("", "-detector-")
+        self.project = create_project("-project-")
+        self.version = create_version("-version-", project=self.project)
 
-        run = experiment.get_run(version)
+    def test_run(self):
+        experiment = Experiment(Experiment.BENCHMARK, self.detector, "-findings_path-", "")
 
-        assert_equals(run._Run__path, join("-findings_path-", "mine_and_detect", "-detector-", "-project-", "-version-"))
+        run = experiment.get_run(self.version)
+
+        assert_equals(run.findings_path,
+                      join("-findings_path-", "mine_and_detect", "-detector-", "-project-", "-version-"))
 
     def test_review_path_top_findings(self):
-        detector = Detector("", "-detector-")
-        experiment = Experiment(Experiment.TOP_FINDINGS, detector, "", "-reviews_path-")
-        project = create_project("-project-")
-        version = create_version("-version-", project=project)
+        experiment = Experiment(Experiment.TOP_FINDINGS, self.detector, "", "-reviews_path-")
 
-        review_path = experiment.get_review_path(version)
+        review_path = experiment.get_review_path(self.version)
 
         assert_equals(review_path,
                       join("-reviews_path-", Experiment.TOP_FINDINGS, "-detector-", "-project-", "-version-"))
 
     def test_review_path_benchmark(self):
-        detector = Detector("", "-detector-")
-        experiment = Experiment(Experiment.BENCHMARK, detector, "", "-reviews_path-")
-        project = create_project("-project-")
-        version = create_version("-version-", project=project)
-        misuse = create_misuse("-misuse-", project=project)
+        experiment = Experiment(Experiment.BENCHMARK, self.detector, "", "-reviews_path-")
+        misuse = create_misuse("-misuse-", project=self.project)
 
-        review_path = experiment.get_review_path(version, misuse)
+        review_path = experiment.get_review_path(self.version, misuse)
 
         assert_equals(review_path,
                       join("-reviews_path-", Experiment.BENCHMARK, "-detector-", "-project-", "-version-", "-misuse-"))
