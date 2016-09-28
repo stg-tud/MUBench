7203b0c03cb1975085ae98b026e55bc0a6c8dd05
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-09-28 17:36:40 +0200

    Cleanup test.

--- benchmark_tests/utils/test_shell.py
+++ benchmark_tests/utils/test_shell.py
@@ -7,23 +7,22 @@ from benchmark.utils.shell import Shell, CommandFailedError
 
 class TestShell:
     def test_runs(self):
-        uut = Shell()
-        uut.exec("echo 'test'")
+        Shell.exec("echo 'test'")
 
     def test_output(self):
-        uut = Shell()
-        out = uut.exec("echo test")
+        out = Shell.exec("echo test")
         assert_equals("test" + os.linesep, out)
 
     def test_command_failure(self):
-        uut = Shell()
         with assert_raises(CommandFailedError):
-            uut.exec("unknown command")
+            Shell.exec("unknown command")
 
     def test_command_try(self):
-        uut = Shell()
-        assert uut.try_exec("echo 'test'")
+        assert Shell.try_exec("echo 'test'")
 
     def test_command_try_failure(self):
-        uut = Shell()
-        assert not uut.try_exec("unknown command")
+        assert not Shell.try_exec("unknown command")
+
+    def test_timeout(self):
+        with assert_raises(TimeoutError):
+            Shell.exec("sleep 10", timeout=1)
