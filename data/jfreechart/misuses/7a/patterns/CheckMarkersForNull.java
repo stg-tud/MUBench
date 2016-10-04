932ad35ceaa4b510a173d70c4ee128acfbc1cf15
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-10-04 15:17:27 +0200

    Fix imports of a hand-crafted pattern.

--- data/jfreechart/misuses/7a/patterns/CheckMarkersForNull.java
+++ data/jfreechart/misuses/7a/patterns/CheckMarkersForNull.java
@@ -1,7 +1,11 @@
+import java.util.Map;
+import java.util.ArrayList;
+
+import org.jfree.chart.plot.Marker;
 
 class CheckMarkersForNull {
-  boolean pattern(Map foregroundDomainMarkers, int index, Marker marker) {
-    ArrayList markers = (ArrayList) foregroundDomainMarkers.get(new Integer(index));
+  boolean pattern(Map domainMarkers, int index, Marker marker) {
+    ArrayList markers = (ArrayList) domainMarkers.get(new Integer(index));
     if (markers == null) {
       return false;
     }
