8d3efd4103aa3b90eb2d26b9c80ee0ca39994982
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-10-07 08:09:22 +0200

    Remove img tag

--- benchmark/data/detector_specialising/specialising_util.py
+++ benchmark/data/detector_specialising/specialising_util.py
@@ -12,7 +12,7 @@ def format_float_value(finding, float_key):
 def replace_dot_graph_with_image(finding, key, base_path) -> str:
     image_name = "f{}-{}.png".format(finding["id"], key)
     __create_image(finding[key], join(base_path, image_name))
-    finding[key] = """<img src="./{}" />""".format(image_name)
+    finding[key] = image_name
     return join(base_path, image_name)
 
 
