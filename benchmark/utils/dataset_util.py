de88c11caedf87e3106ed240386f1e770941cb2f
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2016-10-13 07:55:54 +0200

    Use all data by default

--- benchmark/utils/dataset_util.py
+++ benchmark/utils/dataset_util.py
@@ -1,11 +1,13 @@
-from typing import List
+from typing import List, Optional
 
 from benchmark.utils.io import read_yaml
 
 
-def get_white_list(datasets_file_path: str, dataset_key: str) -> List[str]:
+def get_white_list(datasets_file_path: str, dataset_key: Optional[str]) -> List[str]:
+    if dataset_key is None:
+        return []
+
     datasets = read_yaml(datasets_file_path)
     if dataset_key not in datasets:
         raise ValueError("Invalid dataset: '{}'".format(dataset_key))
     return datasets[dataset_key]
-
