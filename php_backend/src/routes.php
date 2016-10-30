b272e54cd55a0d784c738b7f047e375f67ae26de
Bob the Builder <bob@builder.com>
2016-10-30 11:34:03 +0100

    can read mubench jsons out of request

--- php_backend/src/routes.php
+++ php_backend/src/routes.php
@@ -6,30 +6,36 @@ $app->get('/', function ($request, $response, $args) use ($app) {
 });
 
 $app->post('/api/upload/ex1', function ($request, $response, $args) use ($app) {
-	$json = $request->getUploadedFiles()['json'];
-	$obj = json_decode($json->getStream());
-	//$app->db->handleData('ex1', $obj, $obj->{'potential_hits'});
-	$app->dir->handleImage('ex1', $obj->{'project'}, $obj->{'version'});
-	$this->logger->info('ex1');
+	$obj = json_decode($request->getBody());
+	if($obj){
+		foreach($obj as $d){
+			$app->db->handleData('ex1', $d, $d->{'findings'});
+			//$app->dir->handleImage('ex1', $obj->{'project'}, $obj->{'version'}, $img);
+			$this->logger->info('ex1');
+		}	
+	}
 });
 
 $app->post('/api/upload/ex2', function ($request, $response, $args) use ($app){
-	$this->logger->info('ex2');
-	$files = $request->getUploadedFiles();
-	$img = $files['image'];
-	$json = $files['json'];
-	$img->moveTo('./upload/ex2/'. $img->getClientFilename());
-    $obj = json_decode($json->getStream());
-	$app->db->handleData('ex2', $obj, $obj->{'findings'});
+	$obj = json_decode($request->getBody());
+	if($obj){
+		foreach($obj as $d){
+			$app->db->handleData('ex2', $d, $d->{'findings'});
+			//$app->dir->handleImage('ex1', $obj->{'project'}, $obj->{'version'}, $img);
+			$this->logger->info('ex2');
+		}	
+	}
 });
 
 $app->post('/api/upload/ex3', function ($request, $response, $args) use ($app){
-	$files = $request->getUploadedFiles();
-	$img = $files['image'];
-	$json = $files['json'];
-	$img->moveTo('./upload/ex3/'. $img->getClientFilename());
-    $obj = json_decode($json->getStream());
-    $app->db->handleData('ex3', $obj, $obj->{'potential_hits'});
+    $obj = json_decode($request->getBody());
+	if($obj){
+		foreach($obj as $d){
+			$app->db->handleData('ex3', $d, $d->{'findings'});
+			//$app->dir->handleImage('ex1', $obj->{'project'}, $obj->{'version'}, $img);
+			$this->logger->info('ex3');
+		}	
+	}
 });
 
 $app->post('/api/upload/misuse-metadata/', function ($request, $response, $args) {
