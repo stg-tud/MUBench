574240954d64271f999c1e01b1d6276aa716a4d3
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2016-11-07 15:55:47 +0100

    Refactor tests.

--- benchmark_tests/tasks/implementations/test_publish_metadata_task.py
+++ benchmark_tests/tasks/implementations/test_publish_metadata_task.py
@@ -1,14 +1,27 @@
 from unittest.mock import patch
 
+from nose.tools import assert_equals
+
 from benchmark.tasks.implementations.publish_metadata_task import PublishMetadataTask
 from benchmark_tests.test_utils.data_util import create_misuse, create_project
 
 
+@patch("benchmark.tasks.implementations.publish_metadata_task.post")
 class TestPublishMetadataTask:
+    # noinspection PyAttributeOutsideInit
+    def setup(self):
+        self.project = create_project("-p-")
 
-    @patch("benchmark.tasks.implementations.publish_metadata_task.post")
+    def test_publish_url(self, post_mock):
+        misuse = create_misuse("-m-")
+
+        task = PublishMetadataTask("http://test.url")
+        task.process_project_misuse(self.project, misuse)
+        task.end()
+
+        assert_equals(post_mock.call_args[0][0], "http://test.url/upload/metadata")
+
     def test_publishes_metadata(self, post_mock):
-        project = create_project("-p-")
         misuse = create_misuse("-m-", meta={
             "description": "-description-",
             "fix": {
@@ -23,13 +36,13 @@ class TestPublishMetadataTask:
                 "file": "/some/file.java",
                 "method": "-some.method()-"
             }
-        }, project=project)
+        }, project=self.project)
 
         task = PublishMetadataTask("http://test.url")
-        task.process_project_misuse(project, misuse)
+        task.process_project_misuse(self.project, misuse)
         task.end()
 
-        post_mock.assert_called_with("http://test.url/upload/metadata", [{
+        assert_equals(post_mock.call_args[0][1], [{
             "misuse": misuse.id,
             "description": "-description-",
             "fix": {
