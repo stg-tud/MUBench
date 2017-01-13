87d19e87032f43522e37616470ff4394e293ab17
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-01-13 09:59:00 +0100

    Fix duplicated file name in misuse.

--- data/synthetic/misuses/toorestrictive/patterns/Iterate.java
+++ data/synthetic/misuses/toorestrictive/patterns/Iterate.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public class TooRestrictive {
+public class Iterate {
 	public void pattern(List<String> l) {
 		for (int i = 1; i <= l.size(); i++) {
 			System.out.println("Element #" + i + " is '" + l.get(i -1) + "'");
