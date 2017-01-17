62df660f7401c25ee6ef0957808eb4cb24505c96
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-01-17 14:15:41 +0100

    Use type-safe default for runtime.

--- benchmark/data/detector_execution.py
+++ benchmark/data/detector_execution.py
@@ -55,7 +55,7 @@ class DetectorExecution:
 
         data = {
             "result": None,
-            "runtime": None,
+            "runtime": 0,
             "message": "",
             "md5": None
         }
