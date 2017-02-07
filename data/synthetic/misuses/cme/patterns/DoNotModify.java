e5ec31438b75c57c9b6c9ce22e353f28eaadc7d0
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-02-07 14:37:24 +0100

    Remove add call from CME pattern.

--- data/synthetic/misuses/cme/patterns/DoNotModify.java
+++ data/synthetic/misuses/cme/patterns/DoNotModify.java
@@ -1,9 +1,8 @@
 import java.util.Collection;
 import java.util.Iterator;
 
-public class AddBefore {
+public class DoNotModify {
   public void pattern(Collection<Object> c, Object element) {
-    c.add(element);
   	Iterator<Object> i = c.iterator();
   	if (i.hasNext())
   		i.next();
