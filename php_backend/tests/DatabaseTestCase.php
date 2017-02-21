119d224cd712c1c2f4cebcb258992598b6f6ab92
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2017-02-22 00:51:49 +0100

    sql base db with sql for adding mysql properties

--- php_backend/tests/DatabaseTestCase.php
+++ php_backend/tests/DatabaseTestCase.php
@@ -75,7 +75,7 @@ EOD;
     {
         $this->pdo = new PDO('sqlite::memory:');
         $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-        $structure_sql = file_get_contents(dirname(__FILE__) . "/init_sqlite.sql");
+        $structure_sql = file_get_contents(dirname(__FILE__) . "/../init_db.sql");
         $this->pdo->exec($structure_sql);
         $this->logger = new \Monolog\Logger("test");
         $this->db = new DBConnection($this->pdo, $this->logger);
