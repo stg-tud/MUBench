e124a1c45b70994cf28a68044efef171a5ec3f85
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-03-27 20:29:28 +0200

    Fix test.

--- mubench.reviewsite/tests/SlimTest.php
+++ mubench.reviewsite/tests/SlimTest.php
@@ -4,29 +4,15 @@ require_once "SlimTestCase.php";
 
 class SlimTest extends SlimTestCase
 {
-    public function testIndex(){
+    public function testIndex()
+    {
         $this->get('/');
         $this->assertEquals('200', $this->response->getStatusCode());
     }
-    
-    public function testEx1(){
-        $this->get('/ex1');
-        $this->assertEquals('200', $this->response->getStatusCode());
-    }
 
-    public function testEx2(){
-        $this->get('/ex2');
-        $this->assertEquals('200', $this->response->getStatusCode());
-    }
-
-    public function testEx3(){
-        $this->get('/ex3');
-        $this->assertEquals('200', $this->response->getStatusCode());
-    }
-
-    public function testEx4NotFound(){
+    public function testNotFound()
+    {
         $this->get('/ex4');
         $this->assertEquals('404', $this->response->getStatusCode());
     }
-
 }
