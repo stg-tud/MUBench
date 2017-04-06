49e923409cf4e09f2a8b053eacc68a671a675db7
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-04-06 15:29:08 +0200

    Update detector references.

--- mubench.pipeline/data/detector.py
+++ mubench.pipeline/data/detector.py
@@ -5,6 +5,9 @@ from data.finding import Finding, SpecializedFinding
 
 
 class Detector:
+    BASE_URL = "http://www.st.informatik.tu-darmstadt.de/artifacts/mubench/detectors"
+    CLI_VERSION = "v20170406"
+
     def __init__(self, detectors_path: str, detector_id: str, java_options: List[str]):
         self.id = detector_id
         self.base_name = detector_id.split("_", 1)[0]
@@ -12,7 +15,7 @@ class Detector:
 
         self.path = join(detectors_path, self.id)
         self.jar_path = join(self.path, self.base_name + ".jar")
-        self.jar_url = "http://www.st.informatik.tu-darmstadt.de/artifacts/mubench/{}.jar".format(self.base_name)
+        self.jar_url = "{}/{}/{}.jar".format(Detector.BASE_URL, Detector.CLI_VERSION, self.base_name)
         self.md5_path = join(self.path, self.base_name + ".md5")
 
     @property
