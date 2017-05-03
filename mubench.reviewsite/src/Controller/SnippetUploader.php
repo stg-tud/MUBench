2dbc673a7e65d2e1e01a24e6c90250d78d99420d
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2017-05-03 13:41:06 +0200

    button for snippet deletion on review page added

--- mubench.reviewsite/src/Controller/SnippetUploader.php
+++ mubench.reviewsite/src/Controller/SnippetUploader.php
@@ -31,4 +31,15 @@ class SnippetUploader
         $this->db->table('finding_snippets')->insert(['detector' => $detector->id, 'project' => $project,
             'version' => $version, 'finding' => $misuse, 'snippet' => $snippet, 'line' => $line]);
     }
+
+    public function deleteSnippet($finding){
+        $detector = $this->db->getOrCreateDetector($finding['detector']);
+        $project = $finding['project'];
+        $version = $finding['version'];
+        $misuse = $finding['misuse'];
+        $snippet = $finding['snippet'];
+        $this->logger->info("deleting snippet for $detector, " . $project . ", " . $version . ", " . $misuse);
+        $this->db->table('finding_snippets')->where('detector', $detector->id)->where('project', $project)->where(
+            'version', $version)->where('finding', $misuse)->where('snippet', $snippet)->delete();
+    }
 }
