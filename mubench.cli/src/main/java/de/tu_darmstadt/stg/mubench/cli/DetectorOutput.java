66819ba758269b4437b8c92e5660b8169c200be6
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-05-12 13:46:28 +0200

    tmp

--- mubench.cli/src/main/java/de/tu_darmstadt/stg/mubench/cli/DetectorOutput.java
+++ mubench.cli/src/main/java/de/tu_darmstadt/stg/mubench/cli/DetectorOutput.java
@@ -8,42 +8,20 @@ import de.tu_darmstadt.stg.yaml.YamlObject;
 @SuppressWarnings("WeakerAccess")
 public class DetectorOutput {
 	private final YamlCollection findings = new YamlCollection();
-	private final YamlObject runInfo = new YamlObject();
+	private final YamlObject runInfo;
 
-    /**
-     * Add a finding to the output.
-     * @param file the finding's file location
-     * @param method the findings's method location
-     * @return the new finding
-     */
-	public DetectorFinding add(String file, String method) {
-		DetectorFinding finding = new DetectorFinding(file, method);
-		findings.appendDocument(finding);
-		return finding;
-	}
+    public DetectorOutput(List<DetectorFinding> findings) {
+        this(new YamlObject(), findings);
+    }
+
+    public DetectorOutput(YamlObject runInfo, List<DetectorFinding> findings) {
+        this.runInfo = runInfo;
+        this.findings.appendDocuments(findings);
+    }
 
     YamlCollection getFindings() {
         return findings;
     }
-
-    /**
-     * Add data about the detector run to the output.
-     * @param key identifier of the data
-     * @param value value of the data
-     */
-	public void addRunInformation(String key, String value) {
-		runInfo.put(key, value);
-	}
-
-    /**
-     * Add data about the detector run to the output.
-     * @param key identifier of the data
-     * @param values values of the data
-     */
-	@SuppressWarnings({"unchecked", "unused"})
-	public void addRunInformation(String key, Iterable<String> values) {
-		runInfo.put(key, values);
-	}
 
     YamlObject getRunInfo() {
         return runInfo;
