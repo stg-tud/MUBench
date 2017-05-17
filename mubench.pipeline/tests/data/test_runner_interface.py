4fcb26e6e1cd0a7f2df9a66d46a8611d79e3fadd
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2017-05-17 17:36:26 +0200

    Keep detector arguments in a dictionary until joined with the command

--- mubench.pipeline/tests/data/test_runner_interface.py
+++ mubench.pipeline/tests/data/test_runner_interface.py
@@ -1,4 +1,5 @@
 import logging
+from collections import OrderedDict
 from os.path import join
 from tempfile import mkdtemp
 from unittest import mock
@@ -39,30 +40,30 @@ class TestRunnerInterfaceV20170406:
         original_classpath = join(compiles_path, "original-classes.jar")
         dependencies_classpath = "-dependencies-classpath-"
 
+        detector_args = OrderedDict([
+                ("target", target),
+                ("run_info", run_info),
+                ("detector_mode", "1"),
+                ("training_src_path", training_src_path),
+                ("training_classpath", training_classpath),
+                ("target_src_path", target_src_path),
+                ("target_classpath", target_classpath),
+                ("dep_classpath", dependencies_classpath)
+            ])
+
         uut = RunnerInterfaceV20170406(jar, [])
 
-        uut.execute(self.version,
-            {
-                "target" : target,
-                "run_info" : run_info,
-                "detector_mode" : "1",
-                "training_src_path" : training_src_path,
-                "training_classpath" : training_classpath,
-                "target_src_path" : target_src_path,
-                "target_classpath" : target_classpath,
-                "dep_classpath" : dependencies_classpath
-            },
-            timeout=42, logger=self.logger)
+        uut.execute(self.version, detector_args, timeout=42, logger=self.logger)
 
         shell_mock.exec.assert_called_with('java -jar "{}" '.format(jar) +
-                                           'dep_classpath "{}" '.format(dependencies_classpath) +
-                                           'detector_mode "1" ' +
-                                           'run_info "{}" '.format(run_info) +
                                            'target "{}" '.format(target) +
-                                           'target_classpath "{}" '.format(target_classpath) +
-                                           'target_src_path "{}" '.format(target_src_path) +
+                                           'run_info "{}" '.format(run_info) +
+                                           'detector_mode "1" ' +
+                                           'training_src_path "{}" '.format(training_src_path) +
                                            'training_classpath "{}" '.format(training_classpath) +
-                                           'training_src_path "{}"'.format(training_src_path),
+                                           'target_src_path "{}" '.format(target_src_path) +
+                                           'target_classpath "{}" '.format(target_classpath) +
+                                           'dep_classpath "{}"'.format(dependencies_classpath),
                                            logger=self.logger, timeout=42)
 
     def test_filters_incompatible_args(self, shell_mock):
