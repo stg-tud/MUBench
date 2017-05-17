341a680e5f7ffa1efdfa840aeb3b800da0167f87
Sven Amann <info@sven-amann.de>
2017-05-17 16:24:41 +0200

    Convert dict_keys to List.

--- mubench.pipeline/utils/dataset_util.py
+++ mubench.pipeline/utils/dataset_util.py
@@ -4,7 +4,7 @@ from utils.io import read_yaml
 
 def get_available_datasets(datasets_file_path: str) -> List[str]:
     datasets = read_yaml(datasets_file_path)
-    return datasets.keys()
+    return list(datasets.keys())
 
 def get_white_list(datasets_file_path: str, dataset_key: Optional[str]) -> List[str]:
     if dataset_key is None:
