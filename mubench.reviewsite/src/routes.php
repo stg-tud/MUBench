4df9d37b60610351478860cf208b76afc94c1442
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2017-05-31 18:33:26 +0200

    max reviews setting restricts showed reviews

--- mubench.reviewsite/src/routes.php
+++ mubench.reviewsite/src/routes.php
@@ -18,7 +18,7 @@ $logger = $app->getContainer()['logger'];
 $database = $app->getContainer()['database'];
 $renderer = $app->getContainer()['renderer'];
 // TODO rename RoutesHelper to ResultsViewController
-$routesHelper = new RoutesHelper($database, $renderer, $logger, $settings['upload'], $settings['site_base_url']);
+$routesHelper = new RoutesHelper($database, $renderer, $logger, $settings['upload'], $settings['site_base_url'], $settings['max_reviews']);
 
 $app->get('/', [$routesHelper, 'index']);
 $app->get('/{exp:ex[1-3]}/{detector}', [$routesHelper, 'detector']);
