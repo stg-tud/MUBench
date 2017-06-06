537f2931da256b8cf548d234b012ffd9692ad6ff
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2017-06-06 14:37:41 +0200

    updated tests for possible new stats column

--- mubench.reviewsite/tests/DatabaseTestCase.php
+++ mubench.reviewsite/tests/DatabaseTestCase.php
@@ -28,6 +28,7 @@ class DatabaseTestCase extends TestCase
         "result": "success",
         "runtime": 42.1,
         "number_of_findings": 23,
+        "-custom-stat-": "-stat-val-",
         "potential_hits": [
             {
                 "misuse": "-m-",
--- mubench.reviewsite/tests/StoreDetectorTest.php
+++ mubench.reviewsite/tests/StoreDetectorTest.php
@@ -21,6 +21,7 @@ class StoreDetectorTest extends DatabaseTestCase
         "result": "failure",
         "runtime": 30.1,
         "number_of_findings": 0,
+        "-custom-stat-": "-stat-val-",
         "potential_hits": []
     }
 EOD
