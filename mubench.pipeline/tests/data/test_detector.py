70d22929950bdf0057b74dc11c2633cebca7b8df
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2017-06-07 11:29:25 +0200

    Raise ValueError when no matching release is found

--- mubench.pipeline/tests/data/test_detector.py
+++ mubench.pipeline/tests/data/test_detector.py
@@ -1,7 +1,7 @@
 from os.path import join
 from tempfile import mkdtemp
 
-from nose.tools import assert_equals, assert_is_instance
+from nose.tools import assert_equals, assert_is_instance, assert_raises
 from tests.test_utils.runner_interface_test_impl import RunnerInterfaceTestImpl
 
 from data.detector import Detector
@@ -22,11 +22,8 @@ class TestDetector:
         assert_equals(None, self.detector.md5)
         assert_is_instance(self.detector.runner_interface, NoInterface)
 
-    def test_defaults_on_no_release(self):
-        self.setup_releases([])
-
-        assert_equals(None, self.detector.md5)
-        assert_is_instance(self.detector.runner_interface, NoInterface)
+    def test_raises_value_error_on_no_release(self):
+        assert_raises(ValueError, self.setup_releases, [])
 
     def test_defaults(self):
         self.setup_releases([dict()])
@@ -60,6 +57,11 @@ class TestDetector:
         expected_url = "{}/-release_requested-/-version_requested-/{}.jar".format(Detector.BASE_URL, self.detector.id)
         assert_equals(expected_url, self.detector.jar_url)
         assert_equals("-md5_requested-", self.detector.md5)
+
+    def test_raises_on_no_matching_release(self):
+        assert_raises(ValueError, self.setup_releases,
+                [{"md5": "-md5-", "tag": "-release-", "cli_version": "-version-"}],
+                "-unavailable_release-")
 
     def setup_releases(self, releases, requested_release = None):
         detector_id = "-detector-"
