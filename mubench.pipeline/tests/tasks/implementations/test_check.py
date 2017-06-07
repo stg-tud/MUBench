8b3870f9ad28f99633cac7c54d9e9e3017cd86b7
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2017-06-07 17:26:29 +0200

    Refactoring and test improvements

--- mubench.pipeline/tests/tasks/implementations/test_check.py
+++ mubench.pipeline/tests/tasks/implementations/test_check.py
@@ -4,15 +4,20 @@ from unittest.mock import MagicMock
 from tasks.implementations.check import Check
 from tests.test_utils.data_util import create_project
 
+from requirements import Requirement
 
 class TestCheck():
+    def setup(self):
+        self.uut = Check()
+
+        self.test_requirement = Requirement("-test-requirement-")
+        self.test_requirement.check = MagicMock()
+        self.uut._get_requirements = lambda: [self.test_requirement]
+
     def test_process_project(self):
-        assert_equals([], Check().process_project(create_project("-project-")))
+        assert_equals([], self.uut.process_project(create_project("-project-")))
 
     def test_checks_requirements_on_start(self):
-        uut = Check()
-        uut._check_all_requirements = MagicMock()
+        self.uut.start()
 
-        uut.start()
-
-        uut._check_all_requirements.assert_called_once_with()
+        self.test_requirement.check.assert_called_once_with()
