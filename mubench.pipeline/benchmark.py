5b5ea47f88b1ea27d8c04a76e538fc5ba805c522
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-06-12 10:12:19 +0200

    Fix formatting.

--- mubench.pipeline/benchmark.py
+++ mubench.pipeline/benchmark.py
@@ -102,8 +102,7 @@ class Benchmark:
     def __get_detector(self):
         try:
             java_options = ['-' + option for option in self.config.java_options]
-            return find_detector(self.DETECTORS_PATH, self.config.detector, java_options,
-                    self.config.requested_release)
+            return find_detector(self.DETECTORS_PATH, self.config.detector, java_options, self.config.requested_release)
         except ValueError as e:
             logger.critical(e)
             exit()
