5b2fc21ed0fb31c01a2366b911edd773b2201c83
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2017-06-19 12:57:03 +0200

    fix commit errors

--- mubench.reviewsite/src/settings.php
+++ mubench.reviewsite/src/settings.php
@@ -18,13 +18,13 @@ return [
     ],
     'db' => [
         'driver'    => 'mysql',
-        'host'      => 'localhost:8889',
-        'database'  => 'mubench1',
-        'username'  => 'root',
-        'password'  => 'root',
+        'host'      => 'localhost',
+        'database'  => 'database',
+        'username'  => 'username',
+        'password'  => 'password',
         'charset'   => 'utf8',
         'collation' => 'utf8_unicode_ci',
-        'prefix'    => 'mubench_fse17_',
+        'prefix'    => 'mubench_',
         'options'   => []
     ],
     'upload' => "./upload",
