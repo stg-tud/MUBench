485d8bbc866fa5db52cdcf0d9d329b9573bc7c1b
Mattis Manfred Kaemmerer <mattis.kaemmerer@gmx.de>
2017-06-19 14:23:43 +0200

    Return generic implementation for unknown commands

--- mubench.pipeline/data/build_command.py
+++ mubench.pipeline/data/build_command.py
@@ -5,22 +5,26 @@ from utils.shell import Shell
 
 
 class BuildCommand:
-    def __init__(self, args: List[str]):
+    def __init__(self, name: str, args: List[str]):
+        self._name = name
         self.args = args
 
     @staticmethod
     def get(requested_command: str) -> 'BuildCommand':
         requested_command = requested_command.split(' ')
+        requested_name = requested_command[0]
+        args = requested_command[1:]
 
         commands = BuildCommand._get_implementations()
         matching_commands = [c for c in commands if
-                             c.name() == requested_command[0]]
+                             c.name() == requested_name]
         if len(matching_commands) == 1:
             matching_command = matching_commands[0]
-            args = requested_command[1:]
-            return matching_command(args)
+            return matching_command(requested_name, args)
+        if not matching_commands:
+            return BuildCommand(requested_name, args)
 
-        raise ValueError("None or multiple matching commands found for {}"
+        raise ValueError("Multiple matching commands found for {}"
                          .format(requested_command))
 
     @staticmethod
@@ -40,7 +44,7 @@ class BuildCommand:
         raise NotImplementedError
 
     def _get_command(self, args: List[str]) -> str:
-        return ' '.join([self.name()] + args)
+        return ' '.join([self._name] + args)
 
     def _copy_dependencies(self, output: str, dep_dir: str, compile_base_path: str) -> None:
         pass
