97805c03ecf6a7fc2327ef09f12c96c51c557487
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2017-07-17 13:37:04 +0200

    refactored tests for csv export

--- mubench.reviewsite/tests/CSVHelperTest.php
+++ mubench.reviewsite/tests/CSVHelperTest.php
@@ -14,95 +14,87 @@ class CSVHelperTest extends TestCase
 {
 
     private $csv_helper;
+    private $detector1;
+    private $detector2;
+    private $review1;
+    private $review2;
+    private $no_reviews_misuse;
+    private $positive_reviews_misuse;
+
 
     function setUp()
     {
         $this->csv_helper = new CSVHelper();
+        $this->detector1 = new Detector("-d1-", 1);
+        $this->detector2 = new Detector("-d2-", 2);
+        $this->review1 = new Review([
+            'name' => '-reviewer1-',
+            'comment' => '-comment-',
+            'finding_reviews' => [
+                [
+                    'decision' => 'Yes',
+                    'id' => '0',
+                    'rank' => '0',
+                    'review' => '1',
+                    'violation_types' => [
+                        'missing/call'
+                    ]
+                ]
+            ]
+        ]);
+        $this->review2 = new Review([
+            'name' => '-reviewer2-',
+            'comment' => '-comment-',
+            'finding_reviews' => [
+                [
+                    'decision' => 'Yes',
+                    'id' => '0',
+                    'rank' => '0',
+                    'review' => '1',
+                    'violation_types' => [
+                        'missing/call'
+                    ]
+                ]
+            ]
+        ]);
+        $this->no_reviews_misuse = new Misuse(
+            ["misuse" => "0"],
+            [0 => []
+            ],
+            []
+        );
+        $this->positive_reviews_misuse = new Misuse(
+            ["misuse" => "0"],
+            [0 => []],
+            [$this->review1, $this->review2]
+        );
     }
 
-    public function test_csv_from_stats_ex1()
+    public function test_ex1_stats_as_csv()
     {
-        $stats = [];
-        $stats[1] = new DetectorResult(
-            new Detector("-d1-", 1)
-            , [[
-            "exp" => "ex1",
-            "project" => "-p1-",
-            "version" => "-v-",
-            "result" => "success",
-            "runtime" => "42.1",
-            "number_of_findings" => "23",
-            "misuses" => [new Misuse(
-                ["misuse" => "0", "snippets" => [0 => ["line" => "5", "snippet" => "-code-", "id" => "1"]]],
-                [0 => [
-                    "exp" => "ex2",
-                    "project" => "-p1-",
-                    "version" => "-v-",
-                    "misuse" => "0",
-                    "rank" => "0",
-                    "custom1" => "-val1-",
-                    "custom2" => "-val2-"
-                ]
-                ],
-                []
-            )],
-        ]]);
-        $stats[2] = new DetectorResult(
-            new Detector("-d2-", 1)
-            , [[
-            "exp" => "ex1",
-            "project" => "-p1-",
-            "version" => "-v-",
-            "result" => "success",
-            "runtime" => "42.1",
-            "number_of_findings" => "23",
-            "misuses" => [new Misuse(
-                ["misuse" => "0"],
-                [0 => []],
-                [new Review([
-                    'name' => '-reviewer1-',
-                    'exp' => 'ex1',
-                    'detector' => 1,
-                    'project' => '-p1-',
-                    'version' => '-v-',
-                    'misuse' => '-m-',
-                    'comment' => '-comment-',
-                    'id' => '0',
-                    'finding_reviews' => [
-                        [
-                            'decision' => 'Yes',
-                            'id' => '0',
-                            'rank' => '0',
-                            'review' => '1',
-                            'violation_types' => [
-                                'missing/call'
-                            ]
-                        ]
-                    ]
-                ]),
-                    new Review([
-                        'name' => '-reviewer2-',
-                        'exp' => 'ex1',
-                        'detector' => 1,
-                        'project' => '-p1-',
-                        'version' => '-v-',
-                        'misuse' => '-m-',
-                        'comment' => '-comment-',
-                        'id' => '0',
-                        'finding_reviews' => [
-                            [
-                                'decision' => 'Yes',
-                                'id' => '0',
-                                'rank' => '0',
-                                'review' => '1',
-                                'violation_types' => [
-                                    'missing/call'
-                                ]
-                            ]
-                        ]
-                    ])]
-            )],
-        ]]);
+        $stats = [
+            new DetectorResult(
+                $this->detector1
+                , [[
+                "exp" => "ex1",
+                "project" => "-p1-",
+                "version" => "-v-",
+                "result" => "success",
+                "runtime" => "42.1",
+                "number_of_findings" => "23",
+                "misuses" => [$this->no_reviews_misuse],
+            ]]),
+            new DetectorResult(
+                $this->detector2
+                , [[
+                "exp" => "ex1",
+                "project" => "-p1-",
+                "version" => "-v-",
+                "result" => "success",
+                "runtime" => "42.1",
+                "number_of_findings" => "23",
+                "misuses" => [$this->positive_reviews_misuse],
+            ]])];
         $stats["total"] = new ExperimentResult($stats);
 
         $expected_csv = 'sep=,
@@ -113,89 +105,31 @@ Total,1,0,1,2,2,0,1,0,1,0,0,0,0.5,0,0.5,1,50';
         self::assertEquals($expected_csv, $this->csv_helper->createCSVFromStats("ex1", $stats));
     }
 
-    public function test_csv_from_stats_ex2()
+    public function test_ex2_stats_as_csv()
     {
-        $stats = [];
-        $stats[1] = new DetectorResult(
-            new Detector("-d1-", 1)
-            , [[
-            "exp" => "ex2",
-            "project" => "-p1-",
-            "version" => "-v-",
-            "result" => "success",
-            "runtime" => "42.1",
-            "number_of_findings" => "23",
-            "misuses" => [new Misuse(
-                ["misuse" => "0", "snippets" => [0 => ["line" => "5", "snippet" => "-code-", "id" => "1"]]],
-                [0 => [
-                    "exp" => "ex2",
-                    "project" => "-p1-",
-                    "version" => "-v-",
-                    "misuse" => "0",
-                    "rank" => "0",
-                    "custom1" => "-val1-",
-                    "custom2" => "-val2-"
-                ]
-                ],
-                []
-            )],
-        ]]);
-        $stats[2] = new DetectorResult(
-            new Detector("-d2-", 1)
-            , [[
-            "exp" => "ex2",
-            "project" => "-p1-",
-            "version" => "-v-",
-            "result" => "success",
-            "runtime" => "42.1",
-            "number_of_findings" => "23",
-            "misuses" => [new Misuse(
-                ["misuse" => "0"],
-                [0 => []],
-                [new Review([
-                    'name' => '-reviewer1-',
-                    'exp' => 'ex2',
-                    'detector' => 1,
-                    'project' => '-p1-',
-                    'version' => '-v-',
-                    'misuse' => '-m-',
-                    'comment' => '-comment-',
-                    'id' => '0',
-                    'finding_reviews' => [
-                        [
-                            'decision' => 'Yes',
-                            'id' => '0',
-                            'rank' => '0',
-                            'review' => '1',
-                            'violation_types' => [
-                                'missing/call'
-                            ]
-                        ]
-                    ]
-                ]),
-                    new Review([
-                        'name' => '-reviewer2-',
-                        'exp' => 'ex2',
-                        'detector' => 1,
-                        'project' => '-p1-',
-                        'version' => '-v-',
-                        'misuse' => '-m-',
-                        'comment' => '-comment-',
-                        'id' => '0',
-                        'finding_reviews' => [
-                            [
-                                'decision' => 'Yes',
-                                'id' => '0',
-                                'rank' => '0',
-                                'review' => '1',
-                                'violation_types' => [
-                                    'missing/call'
-                                ]
-                            ]
-                        ]
-                    ])]
-            )],
-        ]]);
+        $stats = [
+            new DetectorResult(
+                $this->detector1
+                , [[
+                "exp" => "ex2",
+                "project" => "-p1-",
+                "version" => "-v-",
+                "result" => "success",
+                "runtime" => "42.1",
+                "number_of_findings" => "23",
+                "misuses" => [$this->no_reviews_misuse],
+            ]]),
+            new DetectorResult(
+                $this->detector2
+                , [[
+                "exp" => "ex2",
+                "project" => "-p1-",
+                "version" => "-v-",
+                "result" => "success",
+                "runtime" => "42.1",
+                "number_of_findings" => "23",
+                "misuses" => [$this->positive_reviews_misuse],
+            ]])];
         $stats["total"] = new ExperimentResult($stats);
 
         $expected_csv = 'sep=,
@@ -206,89 +140,31 @@ Total,1,2,2,0,1,0,1,0,0,0,0.5,0,0.5,1,50';
         self::assertEquals($expected_csv, $this->csv_helper->createCSVFromStats("ex2", $stats));
     }
 
-    public function test_csv_from_stats_ex3()
+    public function test_ex3_stats_as_csv()
     {
-        $stats = [];
-        $stats[1] = new DetectorResult(
-            new Detector("-d1-", 1)
-            , [[
-            "exp" => "ex3",
-            "project" => "-p1-",
-            "version" => "-v-",
-            "result" => "success",
-            "runtime" => "42.1",
-            "number_of_findings" => "23",
-            "misuses" => [new Misuse(
-                ["misuse" => "0", "snippets" => [0 => ["line" => "5", "snippet" => "-code-", "id" => "1"]]],
-                [0 => [
-                    "exp" => "ex2",
-                    "project" => "-p1-",
-                    "version" => "-v-",
-                    "misuse" => "0",
-                    "rank" => "0",
-                    "custom1" => "-val1-",
-                    "custom2" => "-val2-"
-                ]
-                ],
-                []
-            )],
-        ]]);
-        $stats[2] = new DetectorResult(
-            new Detector("-d2-", 1)
-            , [[
-            "exp" => "ex3",
-            "project" => "-p1-",
-            "version" => "-v-",
-            "result" => "success",
-            "runtime" => "42.1",
-            "number_of_findings" => "23",
-            "misuses" => [new Misuse(
-                ["misuse" => "0"],
-                [0 => []],
-                [new Review([
-                    'name' => '-reviewer1-',
-                    'exp' => 'ex3',
-                    'detector' => 1,
-                    'project' => '-p1-',
-                    'version' => '-v-',
-                    'misuse' => '-m-',
-                    'comment' => '-comment-',
-                    'id' => '0',
-                    'finding_reviews' => [
-                        [
-                            'decision' => 'Yes',
-                            'id' => '0',
-                            'rank' => '0',
-                            'review' => '1',
-                            'violation_types' => [
-                                'missing/call'
-                            ]
-                        ]
-                    ]
-                ]),
-                    new Review([
-                        'name' => '-reviewer2-',
-                        'exp' => 'ex3',
-                        'detector' => 1,
-                        'project' => '-p1-',
-                        'version' => '-v-',
-                        'misuse' => '-m-',
-                        'comment' => '-comment-',
-                        'id' => '0',
-                        'finding_reviews' => [
-                            [
-                                'decision' => 'Yes',
-                                'id' => '0',
-                                'rank' => '0',
-                                'review' => '1',
-                                'violation_types' => [
-                                    'missing/call'
-                                ]
-                            ]
-                        ]
-                    ])]
-            )],
-        ]]);
+        $stats = [
+            new DetectorResult(
+                $this->detector1
+                , [[
+                "exp" => "ex3",
+                "project" => "-p1-",
+                "version" => "-v-",
+                "result" => "success",
+                "runtime" => "42.1",
+                "number_of_findings" => "23",
+                "misuses" => [$this->no_reviews_misuse],
+            ]]),
+            new DetectorResult(
+                $this->detector2
+                , [[
+                "exp" => "ex3",
+                "project" => "-p1-",
+                "version" => "-v-",
+                "result" => "success",
+                "runtime" => "42.1",
+                "number_of_findings" => "23",
+                "misuses" => [$this->positive_reviews_misuse],
+            ]])];
         $stats["total"] = new ExperimentResult($stats);
 
         $expected_csv = 'sep=,
@@ -300,7 +176,7 @@ Total,1,1,2,2,0,1,0,1,0,0,0,0.5,0,0.5,1,50';
     }
 
 
-    function test_csv_from_detector()
+    function test_export_detector_run_as_csv()
     {
         $runs = [[
             "exp" => "ex1",
@@ -309,57 +185,12 @@ Total,1,1,2,2,0,1,0,1,0,0,0,0.5,0,0.5,1,50';
             "result" => "success",
             "runtime" => "42.1",
             "number_of_findings" => "23",
-            "misuses" => [new Misuse(
-                ["misuse" => "0"],
-                [0 => []],
-                [new Review([
-                    'name' => '-reviewer1-',
-                    'exp' => 'ex3',
-                    'detector' => 1,
-                    'project' => '-p1-',
-                    'version' => '-v-',
-                    'misuse' => '-m-',
-                    'comment' => '-comment-',
-                    'id' => '0',
-                    'finding_reviews' => [
-                        [
-                            'decision' => 'Yes',
-                            'id' => '0',
-                            'rank' => '0',
-                            'review' => '1',
-                            'violation_types' => [
-                                'missing/call'
-                            ]
-                        ]
-                    ]
-                ]),
-                    new Review([
-                        'name' => '-reviewer2-',
-                        'exp' => 'ex3',
-                        'detector' => 1,
-                        'project' => '-p1-',
-                        'version' => '-v-',
-                        'misuse' => '-m-',
-                        'comment' => '-comment-',
-                        'id' => '0',
-                        'finding_reviews' => [
-                            [
-                                'decision' => 'Yes',
-                                'id' => '0',
-                                'rank' => '0',
-                                'review' => '1',
-                                'violation_types' => [
-                                    'missing/call'
-                                ]
-                            ]
-                        ]
-                    ])]
-            )],
-            "-custom-stat-" => "-stat-val-",
+            "misuses" => [$this->positive_reviews_misuse, $this->no_reviews_misuse],
         ]];
         $expected_csv = 'sep=,
 project,version,result,number_of_findings,runtime,misuse,decision,resolution_decision,resolution_comment,review1_name,review1_decision,review1_comment
--p-,-v-,success,23,42.1,0,4,,,-reviewer1-,2,-comment-,-reviewer2-,2,-comment-';
+-p-,-v-,success,23,42.1,0,4,,,-reviewer1-,2,-comment-,-reviewer2-,2,-comment-
+-p-,-v-,success,23,42.1,0,1,,';
         self::assertEquals($expected_csv, $this->csv_helper->createCSVFromRuns($runs));
     }
 
