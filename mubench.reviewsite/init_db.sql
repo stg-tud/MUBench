6122f2784c3d6090d2a466a24babc44cd5dd41aa
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2017-08-16 11:18:16 +0200

    fixed sql for tests

--- mubench.reviewsite/init_db.sql
+++ mubench.reviewsite/init_db.sql
@@ -80,8 +80,8 @@ CREATE TABLE IF NOT EXISTS `misuse_types` (
   `version` varchar(100) NOT NULL,
   `misuse` varchar(100) NOT NULL,
   `type` int(11) NOT NULL,
-  KEY `lookup` (`project`,`version`,`misuse`)
-  UNIQUE KEY (`project`,`version`,`misuse`, `type`)
+  KEY `lookup` (`project`,`version`,`misuse`),
+  UNIQUE KEY `misuse_type` (`project`,`version`,`misuse`, `type`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
 
 CREATE TABLE IF NOT EXISTS `types` (
