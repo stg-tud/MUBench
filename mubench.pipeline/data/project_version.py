dbe90bdff9503b19b563ce18791ea63f5f3b5ae9
M8is <mattis.kaemmerer@gmx.de>
2017-08-29 09:20:27 +0200

    Remove obsolete warning

--- mubench.pipeline/data/project_version.py
+++ mubench.pipeline/data/project_version.py
@@ -82,8 +82,6 @@ class ProjectVersion:
     def misuses(self) -> List[Misuse]:
         if not self._MISUSES:
             misuse_ids = self._yaml.get("misuses", []) or []
-            if not misuse_ids:
-                logging.getLogger("version").warning("!! %s has no misuses!", self)
             self._MISUSES = [Misuse(self._base_path, self.__project.id, misuse_id) for misuse_id in misuse_ids
                              if Misuse.is_misuse(join(self._misuses_dir, misuse_id))]
 
