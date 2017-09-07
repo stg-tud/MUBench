7f628693bfa98693030cf059f3f57d1c211bf464
M8is <mattis.kaemmerer@gmx.de>
2017-09-07 08:55:48 +0200

    Move comment into changelog to avoid confusion

--- mubench.pipeline/data/runner_interface.py
+++ mubench.pipeline/data/runner_interface.py
@@ -197,4 +197,4 @@ class RunnerInterface_0_0_10(RunnerInterface_0_0_8):
 
     @staticmethod
     def changelog():
-        return ""  # only changed Java-side interface
+        return "Only changed Java-side interface."
