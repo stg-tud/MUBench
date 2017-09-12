429bcd952d4d8b793b41a88432380c786704b4dc
Sven Amann <info@sven-amann.de>
2017-09-12 09:00:07 +0200

    Add test for encoding of in-range float.

--- mubench.pipeline/tests/utils/test_json_float_encoder.py
+++ mubench.pipeline/tests/utils/test_json_float_encoder.py
@@ -19,3 +19,8 @@ class TestJSONFloatEncoder:
         result = json.dumps(float('-inf'), cls=JSONFloatEncoder)
 
         assert_equals('-Infinity', result)
+
+    def test_encodes_float_as_string(self):
+        result = json.dumps(42.1337, cls=JSONFloatEncoder)
+
+        assert_equals('42.1337', result)
