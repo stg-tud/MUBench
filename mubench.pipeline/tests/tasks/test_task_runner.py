eb45659d9252a733e4633ecee4c1443022726d18
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-09-27 14:00:11 +0200

    Chose task parameters according to formal parameter types.

--- mubench.pipeline/tests/tasks/test_task_runner.py
+++ mubench.pipeline/tests/tasks/test_task_runner.py
@@ -1,47 +1,82 @@
-from unittest.mock import MagicMock, call
+from nose.tools import assert_in
+from typing import List
 
 from tasks.task_runner import TaskRunner
 
 
 class TestTaskRunner:
     def test_runs_first_task(self):
-        task = MagicMock()
+        task = VoidTask()
         uut = TaskRunner([task])
 
         uut.run()
 
-        task.run.assert_called_once_with()
+        task.assert_called_once_with()
 
     def test_runs_second_task_with_result_of_first_task(self):
-        first_task = MagicMock()
-        first_task.run.return_value = [":some string:"]
-        second_task = MagicMock()
+        first_task = VoidTask([":some string:"])
+        second_task = StringConsumingTask()
         uut = TaskRunner([first_task, second_task])
 
         uut.run()
 
-        second_task.run.assert_called_once_with(":some string:")
+        second_task.assert_called_once_with(":some string:")
 
     def test_runs_second_task_with_each_result_of_first_task(self):
-        first_task = MagicMock()
-        first_task.run.return_value = [":some string:", ":other string:"]
-        second_task = MagicMock()
+        first_task = VoidTask([":some string:", ":other string:"])
+        second_task = StringConsumingTask()
         uut = TaskRunner([first_task, second_task])
 
         uut.run()
 
-        # Would like to ensure order here, but calls contains additional elements 'call().__iter__()` (transitive calls
-        # on the result of run()), which we cannot even specify in the test.
-        second_task.run.assert_has_calls([call(":some string:"), call(":other string:")], any_order=True)
+        second_task.assert_has_calls([(":some string:",), (":other string:",)])
 
     def test_runs_subsequent_task_with_results_of_all_previous_tasks(self):
-        first_task = MagicMock()
-        first_task.run.return_value = [":some string:"]
-        second_task = MagicMock()
-        second_task.run.return_value = [42]
-        third_task = MagicMock()
+        first_task = VoidTask([":some string:"])
+        second_task = StringConsumingTask([42])
+        third_task = StringAndIntConsumingTask()
         uut = TaskRunner([first_task, second_task, third_task])
 
         uut.run()
 
-        third_task.run.assert_called_once_with(":some string:", 42)
+        third_task.assert_called_once_with(":some string:", 42)
+
+    def test_drops_previous_results_if_subsequent_task_does_not_declare_respective_parameter(self):
+        first_task = VoidTask([":some string:"])
+        second_task = VoidTask()
+        uut = TaskRunner([first_task, second_task])
+
+        uut.run()
+
+        second_task.assert_called_once_with()
+
+
+class MockTask:
+    def __init__(self, results: List = None):
+        self.results = results or []
+        self.calls = []
+
+    def assert_called_once_with(self, *args):
+        self.assert_has_calls([args])
+
+    def assert_has_calls(self, calls):
+        for call in calls:
+            assert_in(call, self.calls)
+
+
+class VoidTask(MockTask):
+    def run(self):
+        self.calls.append(())
+        return self.results
+
+
+class StringConsumingTask(MockTask):
+    def run(self, s: str):
+        self.calls.append((s,))
+        return self.results
+
+
+class StringAndIntConsumingTask(MockTask):
+    def run(self, s: str, i: int):
+        self.calls.append((s, i))
+        return self.results
