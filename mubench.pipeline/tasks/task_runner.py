0d2eedc236439c667bec361b6b44ba9fe28df78a
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2017-09-27 14:00:11 +0200

    Chose task parameters according to formal parameter types.

--- mubench.pipeline/tasks/task_runner.py
+++ mubench.pipeline/tasks/task_runner.py
@@ -1,4 +1,6 @@
-from typing import List, Dict
+from inspect import signature, Parameter
+
+from typing import List
 
 
 class TaskRunner:
@@ -10,12 +12,37 @@ class TaskRunner:
 
     def __run(self, current_task_index: int, previous_results: List):
         task = self.tasks[current_task_index]
-
-        if previous_results:
-            results = task.run(*previous_results)
-        else:
-            results = task.run()
-
+        parameter_values = self.__get_parameter_values(task, previous_results)
+        results = task.run(*parameter_values)
         for result in results:
             next_results = previous_results + [result]
             self.__run(current_task_index + 1, next_results)
+
+    @staticmethod
+    def __get_parameter_values(task, previous_results):
+        parameter_values = []
+        for parameter in TaskRunner.__get_parameters(task):
+            parameter_value = TaskRunner.__find_value(parameter, previous_results)
+            if parameter_value:
+                parameter_values.append(parameter_value)
+            else:
+                raise TaskParameterUnavailableWarning(task, parameter)
+        return parameter_values
+
+    @staticmethod
+    def __get_parameters(task):
+        return signature(task.run).parameters.values()
+
+    @staticmethod
+    def __find_value(parameter: Parameter, previous_results):
+        for value in previous_results:
+            if parameter.annotation == type(value):
+                return value
+        return None
+
+
+class TaskParameterUnavailableWarning(UserWarning):
+    def __init__(self, task, parameter: Parameter):
+        super().__init__("Missing parameter {} for task {}".format(parameter.name, type(task).__name__))
+        self.task = task
+        self.parameter = parameter
