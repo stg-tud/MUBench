c49f6997d73db7e78c0067c9950de839bcf5620d
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2017-10-11 12:28:45 +0200

    Use data filter in data collecting tasks

--- mubench.pipeline/tasks/implementations/collect_misuses.py
+++ mubench.pipeline/tasks/implementations/collect_misuses.py
@@ -1,7 +1,8 @@
 from data.project_version import ProjectVersion
+from utils.data_filter import DataFilter
 
 
 class CollectMisuses:
     # noinspection PyMethodMayBeStatic
-    def run(self, version: ProjectVersion):
-        return version.misuses
+    def run(self, data_filter: DataFilter, version: ProjectVersion):
+        return [misuse for misuse in version.misuses if not data_filter.is_filtered(misuse.id)]
--- mubench.pipeline/tests/tasks/implementations/test_collect_projects.py
+++ mubench.pipeline/tests/tasks/implementations/test_collect_projects.py
@@ -1,10 +1,15 @@
 from tempfile import mkdtemp
+from unittest.mock import MagicMock
 
 from nose.tools import assert_equals
 
 from tasks.implementations.collect_projects import CollectProjects
 from tests.test_utils.data_util import create_project
+from utils.data_filter import DataFilter
 from utils.io import remove_tree, create_file
+
+NO_FILTER = MagicMock()  # type: DataFilter
+NO_FILTER.is_filtered = lambda id_: False
 
 
 class TestCollectProject:
@@ -21,6 +26,17 @@ class TestCollectProject:
         create_file(p2._project_file)
         uut = CollectProjects(self.temp_dir)
 
-        actual = uut.run()
+        actual = uut.run(NO_FILTER)
 
         assert_equals([p1, p2], actual)
+
+    def test_uses_filter(self):
+        project = create_project("-id-", base_path=self.temp_dir)
+        create_file(project._project_file)
+        uut = CollectProjects(self.temp_dir)
+        filter_ = MagicMock()  # type: DataFilter
+        filter_.is_filtered = lambda id_: id_ == "-id-"
+
+        actual = uut.run(filter_)
+
+        assert_equals([], actual)
