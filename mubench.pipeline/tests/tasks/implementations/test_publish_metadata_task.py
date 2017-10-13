974f3b4cbde3789591b43ff3d48dc5bc0bacf09f
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2017-10-13 12:57:34 +0200

    Adapt publish metadata to new task runner

--- mubench.pipeline/tests/tasks/implementations/test_publish_metadata_task.py
+++ mubench.pipeline/tests/tasks/implementations/test_publish_metadata_task.py
@@ -4,12 +4,12 @@ from nose.tools import assert_equals
 
 from data.pattern import Pattern
 from data.snippets import Snippet
-from tasks.implementations.publish_metadata_task import PublishMetadataTask
+from tasks.implementations.publish_metadata import PublishMetadata
 from tests.test_utils.data_util import create_misuse, create_project, create_version
 
 
 @patch("data.misuse.get_snippets")
-@patch("tasks.implementations.publish_metadata_task.post")
+@patch("tasks.implementations.publish_metadata.post")
 class TestPublishMetadataTask:
     # noinspection PyAttributeOutsideInit
     def setup(self):
@@ -19,35 +19,33 @@ class TestPublishMetadataTask:
         misuse = create_misuse("-m-", project=self.project)
         create_version("-v-", project=self.project, misuses=[misuse])
 
-        task = PublishMetadataTask("-compiles-path-", "http://test.url")
-        task.process_project_misuse(self.project, misuse)
+        task = PublishMetadata("-compiles-path-", "http://test.url")
+        task.run(self.project, misuse)
         task.end()
 
         assert_equals(post_mock.call_args[0][0], "http://test.url/api/upload/metadata")
 
-    @patch("tasks.implementations.publish_findings_task.getpass.getpass")
+    @patch("tasks.implementations.publish_metadata.getpass.getpass")
     def test_post_auth_prompt(self, pass_mock, post_mock, snippets_mock):
         misuse = create_misuse("-m-", project=self.project)
         create_version("-v-", project=self.project, misuses=[misuse])
         pass_mock.return_value = "-password-"
 
-        task = PublishMetadataTask("-compiles-path-", "http://test.url", "-username-")
-        task.start()  # should ask for password once
-        task.process_project_misuse(self.project, misuse)
+        task = PublishMetadata("-compiles-path-", "http://test.url", "-username-")
+        task.run(self.project, misuse)
         task.end()
 
         assert_equals(post_mock.call_args[1]["username"], "-username-")
         assert_equals(post_mock.call_args[1]["password"], "-password-")
 
-    @patch("tasks.implementations.publish_findings_task.getpass.getpass")
+    @patch("tasks.implementations.publish_metadata.getpass.getpass")
     def test_post_auth_provided(self, pass_mock, post_mock, snippets_mock):
         misuse = create_misuse("-m-", project=self.project)
         create_version("-v-", project=self.project, misuses=[misuse])
         pass_mock.side_effect = UserWarning("should skip prompt")
 
-        task = PublishMetadataTask("-compiles-path-", "http://test.url", "-username-", "-password-")
-        task.start()  # should not ask for password, since already set
-        task.process_project_misuse(self.project, misuse)
+        task = PublishMetadata("-compiles-path-", "http://test.url", "-username-", "-password-")
+        task.run(self.project, misuse)
         task.end()
 
         assert_equals(post_mock.call_args[1]["username"], "-username-")
@@ -72,8 +70,8 @@ class TestPublishMetadataTask:
         create_version("-v-", project=self.project, misuses=[misuse])
         snippets_mock.return_value = [Snippet("-code-", 42)]
 
-        task = PublishMetadataTask("-compiles-path-", "http://test.url")
-        task.process_project_misuse(self.project, misuse)
+        task = PublishMetadata("-compiles-path-", "http://test.url")
+        task.run(self.project, misuse)
         task.end()
 
         assert_equals(post_mock.call_args[0][1], [{
@@ -97,7 +95,7 @@ class TestPublishMetadataTask:
             "patterns": []
         }])
 
-    @patch("tasks.implementations.publish_metadata_task.safe_read")
+    @patch("tasks.implementations.publish_metadata.safe_read")
     def test_publishes_pattern_code(self, read_mock, post_mock, snippets_mock):
         pattern_code = "public class P1 {\n" \
             "  void m() { return; }\n" \
@@ -106,15 +104,15 @@ class TestPublishMetadataTask:
         misuse = create_misuse("-m-", project=self.project, patterns=[Pattern("/base/path", "P1.java")])
         create_version("-v-", project=self.project, misuses=[misuse])
 
-        task = PublishMetadataTask("-compiles-path-", "http://test.url")
-        task.process_project_misuse(self.project, misuse)
+        task = PublishMetadata("-compiles-path-", "http://test.url")
+        task.run(self.project, misuse)
         task.end()
 
         assert_equals(post_mock.call_args[0][1][0]["patterns"], [
             {"id": "P1", "snippet": {"code": pattern_code, "first_line": 1}}
         ])
 
-    @patch("tasks.implementations.publish_metadata_task.safe_read")
+    @patch("tasks.implementations.publish_metadata.safe_read")
     def test_publishes_pattern_code_without_preamble(self, read_mock, post_mock, snippets_mock):
         pattern_preamble = "package foo;\n" \
                        "import Bar;\n" \
@@ -126,21 +124,21 @@ class TestPublishMetadataTask:
         misuse = create_misuse("-m-", project=self.project, patterns=[Pattern("/", "P1.java")])
         create_version("-v-", project=self.project, misuses=[misuse])
 
-        task = PublishMetadataTask("-compiles-path-", "http://test.url")
-        task.process_project_misuse(self.project, misuse)
+        task = PublishMetadata("-compiles-path-", "http://test.url")
+        task.run(self.project, misuse)
         task.end()
 
         assert_equals(post_mock.call_args[0][1][0]["patterns"][0]["snippet"]["code"], pattern_code)
 
-    @patch("tasks.implementations.publish_metadata_task.safe_read")
+    @patch("tasks.implementations.publish_metadata.safe_read")
     def test_publishes_pattern_code_without_trailing_newlines(self, read_mock, post_mock, snippets_mock):
         pattern_code = "public class P1 {}"
         read_mock.return_value = pattern_code + "\n\n\n"
         misuse = create_misuse("-m-", project=self.project, patterns=[Pattern("/", "P1.java")])
         create_version("-v-", project=self.project, misuses=[misuse])
 
-        task = PublishMetadataTask("-compiles-path-", "http://test.url")
-        task.process_project_misuse(self.project, misuse)
+        task = PublishMetadata("-compiles-path-", "http://test.url")
+        task.run(self.project, misuse)
         task.end()
 
         assert_equals(post_mock.call_args[0][1][0]["patterns"][0]["snippet"]["code"], pattern_code)
