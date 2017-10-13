9163fbdb8f18f5f1b046d9862b95245af7e41a1c
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2017-10-13 12:54:32 +0200

    Adapt publish findings to new task runner

--- mubench.pipeline/tasks/implementations/publish_findings.py
+++ mubench.pipeline/tasks/implementations/publish_findings.py
@@ -8,13 +8,14 @@ from requests import RequestException
 from data.experiments import Experiment
 from data.finding import SpecializedFinding
 from data.project import Project
+from data.project_compile import ProjectCompile
 from data.project_version import ProjectVersion
+from data.run import Run
 from data.snippets import SnippetUnavailableException
-from tasks.project_version_task import ProjectVersionTask
 from utils.web_util import post, as_markdown
 
 
-class PublishFindingsTask(ProjectVersionTask):
+class PublishFindings:
     def __init__(self, experiment: Experiment, dataset: str, compiles_base_path: str, review_site_url: str,
                  review_site_user: str= "", review_site_password: str= ""):
         super().__init__()
@@ -31,7 +32,6 @@ class PublishFindingsTask(ProjectVersionTask):
 
         self.logger = logging.getLogger("review_findings")
 
-    def start(self):
         if self.review_site_user and not self.review_site_password:
             self.review_site_password = getpass.getpass(
                 "Enter review-site password for '{}': ".format(self.review_site_user))
@@ -39,10 +39,10 @@ class PublishFindingsTask(ProjectVersionTask):
         self.logger.info("Prepare findings of %s in %s for upload to %s...",
                          self.detector, self.experiment, self.__upload_url)
 
-    def process_project_version(self, project: Project, version: ProjectVersion) -> List[str]:
+    def run(self, project: Project, version: ProjectVersion, detector_run: Run,
+            version_compile: ProjectCompile) -> List:
         logger = self.logger.getChild("version")
 
-        detector_run = self.experiment.get_run(version)
         run_info = detector_run.get_run_info()
         potential_hits = detector_run.get_potential_hits()
 
@@ -61,8 +61,6 @@ class PublishFindingsTask(ProjectVersionTask):
                 logger.info("Not run on %s.", version)
                 result = "not run"
 
-        version_compile = version.get_compile(self.compiles_base_path)
-
         try:
             logger.info("Publishing findings...")
             for potential_hits_slice in self.__slice_by_max_files_per_post(potential_hits):
@@ -71,7 +69,7 @@ class PublishFindingsTask(ProjectVersionTask):
                     postable_data = self._prepare_post(potential_hit, version_compile, logger)
                     post_data_slice.append(postable_data)
 
-                file_paths = PublishFindingsTask.get_file_paths(potential_hits_slice)
+                file_paths = PublishFindings.get_file_paths(potential_hits_slice)
                 self.__post(project, version, run_info, result, post_data_slice, file_paths)
             logger.info("Findings published.")
         except RequestException as e:
@@ -81,7 +79,7 @@ class PublishFindingsTask(ProjectVersionTask):
             else:
                 logger.error("ERROR: %s", e)
 
-        return self.ok()
+        return []
 
     def __slice_by_max_files_per_post(self, potential_hits: List[SpecializedFinding]) -> List[List[SpecializedFinding]]:
         potential_hits_slice = []
