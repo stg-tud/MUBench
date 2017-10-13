b48325506c764e45095e6598ca260781cec1c972
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2017-10-13 12:28:27 +0200

    Adapt detect to new task runner

--- mubench.pipeline/tasks/implementations/detect.py
+++ mubench.pipeline/tasks/implementations/detect.py
@@ -4,14 +4,13 @@ from typing import Optional, List
 from urllib.error import URLError
 
 from data.detector import Detector
-from data.experiments import Experiment, ProvidedPatternsExperiment
-from data.project import Project
+from data.experiments import Experiment
 from data.project_version import ProjectVersion
-from tasks.project_version_task import ProjectVersionTask
+from data.run import Run
 from utils.web_util import download_file
 
 
-class Detect(ProjectVersionTask):
+class Detect:
     def __init__(self, compiles_base_path: str, experiment: Experiment, timeout: Optional[int], force_detect: bool):
         super().__init__()
         self.force_detect = force_detect
@@ -27,9 +26,8 @@ class Detect(ProjectVersionTask):
         self.key_training_classpath = "training_classpath"
         self.key_target_src_path = "target_src_path"
         self.key_target_classpath = "target_classpath"
+        self.logger = logging.getLogger("detect")
 
-    def start(self):
-        logger = logging.getLogger("detect")
         msg = "Running '{}' detector".format(self.detector)
         if self.force_detect:
             msg += " with forced detection and"
@@ -37,7 +35,7 @@ class Detect(ProjectVersionTask):
             msg += " with a timeout of {}s".format(self.timeout)
         else:
             msg += " without timeout."
-        logger.info(msg)
+        self.logger.info(msg)
 
         if not self._detector_available():
             self._download()
@@ -46,9 +44,8 @@ class Detect(ProjectVersionTask):
         return exists(self.detector.jar_path)
 
     def _download(self):
-        logger = logging.getLogger("detect")
         url = self.detector.jar_url
-        logger.info("Loading detector from '%s'...", url)
+        self.logger.info("Loading detector from '%s'...", url)
         file = self.detector.jar_path
 
         try:
@@ -57,26 +54,25 @@ class Detect(ProjectVersionTask):
                 raise ValueError("Missing MD5 for {}".format(self.detector.id))
             download_file(url, file, md5)
         except (FileNotFoundError, ValueError, URLError) as e:
-            logger.error("Download failed: %s", e)
+            self.logger.error("Download failed: %s", e)
             exit(1)
 
-    def process_project_version(self, project: Project, version: ProjectVersion) -> List[str]:
-        logger = logging.getLogger("detect")
+    def run(self, version: ProjectVersion) -> List[Run]:
         run = self.experiment.get_run(version)
 
         if run.is_outdated() or self.force_detect:
             pass
         elif run.is_error():
-            logger.info("Error in previous %s. Skipping.", run)
-            return self.skip(version)
+            self.logger.info("Error in previous %s. Skipping.", run)
+            return []
         elif run.is_success():
-            logger.info("Successful previous %s. Skipping.", run)
-            return self.ok()
+            self.logger.info("Successful previous %s. Skipping.", run)
+            return [run]
 
         run.reset()
 
-        logger.info("Executing %s...", run)
+        self.logger.info("Executing %s...", run)
         logger = logging.getLogger("detect.run")
         run.execute(self.compiles_base_path, self.timeout, logger)
 
-        return self.ok() if run.is_success() else self.skip(version)
+        return [run] if run.is_success() else []
