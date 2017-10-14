7c097a2512495f42f379d283c46e3d2b796e5212
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2017-10-14 13:09:26 +0200

    Fix whitelisting of projects and versions

--- mubench.pipeline/utils/data_filter.py
+++ mubench.pipeline/utils/data_filter.py
@@ -8,5 +8,9 @@ class DataFilter:
 
     def is_filtered(self, id_: str):
         blacklisted = id_ in self.black_list
-        whitelisted = not self.white_list or id_ in self.white_list
+        whitelisted = not self.white_list or self._is_whitelisted(id_)
         return blacklisted or not whitelisted
+
+    def _is_whitelisted(self, id_: str):
+        return id_ in self.white_list if '.' not in id_ \
+            else self._is_whitelisted(id_.rsplit('.', 1)[0])
