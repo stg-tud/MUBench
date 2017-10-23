e6ea8c5ffbd126783278c603cb0c611150e53c14
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2017-10-23 13:03:28 +0200

    Delete old task runner and base classes

--- mubench.pipeline/crossproject_create_index.py
+++ mubench.pipeline/crossproject_create_index.py
@@ -3,17 +3,23 @@ from typing import List
 from data.misuse import Misuse
 from data.project import Project
 from data.project_version import ProjectVersion
-from task_runner import TaskRunner
-from tasks.project_version_misuse_task import ProjectVersionMisuseTask
+from tasks.implementations.collect_misuses import CollectMisuses
+from tasks.implementations.collect_projects import CollectProjects
+from tasks.implementations.collect_versions import CollectVersions
+from tasks.task_runner import TaskRunner
+from utils.data_filter import DataFilter
 from utils.dataset_util import get_white_list
 
 
-class Task(ProjectVersionMisuseTask):
+class Task:
     def process_project_version_misuse(self, project: Project, version: ProjectVersion, misuse: Misuse) -> List[str]:
         print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(project.id, version.version_id, misuse.misuse_id, version.source_dir, misuse.location.file, misuse.location.method, misuse.apis[0]))
-        return self.ok()
+        return [self]
+
 
 white_list = get_white_list("../data/datasets.yml", "icse16ex1")
-runner = TaskRunner("../data/", white_list, [])
-runner.add(Task())
+data_filter = DataFilter(white_list, [])
+runner = TaskRunner(
+    [CollectProjects("../data/", data_filter), CollectVersions(data_filter), CollectMisuses(data_filter),
+     Task()])
 runner.run()
