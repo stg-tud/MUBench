6b38e5eb3cf335da2320bc2a11333eada3ed9d90
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-01-22 11:40:37 +0100

    Enable compile for Battleforge project.

--- data/battleforge/misuses/3/patterns/DecodeAfterLoad.java
+++ data/battleforge/misuses/3/patterns/DecodeAfterLoad.java
@@ -1,11 +1,18 @@
+import java.security.InvalidKeyException;
 import java.security.Key;
+import java.security.NoSuchAlgorithmException;
 
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+import java.io.UnsupportedEncodingException;
 
 import org.apache.axis.encoding.Base64;
 
 class DecodeAfterLoad {
-  String pattern(Key key, String value) {
+  String pattern(Key key, String value) throws BadPaddingException, InvalidKeyException, IllegalBlockSizeException, NoSuchAlgorithmException, NoSuchPaddingException, UnsupportedEncodingException {
     Cipher c = Cipher.getInstance("AES");
     c.init(Cipher.DECRYPT_MODE, key);
     return new String(c.doFinal(Base64.decode(value)), "UTF8");
