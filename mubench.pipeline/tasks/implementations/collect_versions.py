6bf0be4d8d434809f15d903e42002f0f772870b1
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2018-01-29 15:19:08 +0100

    Fix filter logic

--- mubench.pipeline/tasks/implementations/collect_versions.py
+++ mubench.pipeline/tasks/implementations/collect_versions.py
@@ -9,19 +9,15 @@ class CollectVersionsTask:
 
     def run(self, project: Project, data_entity_lists: DataEntityLists):
         return [version for version in project.versions
-                if not self.__is_filtered(project.id, version, data_entity_lists, self._filter_non_compilable_versions)]
+                if not self.__is_filtered(project.id, version, data_entity_lists)]
 
-    @staticmethod
-    def __is_filtered(project_id: str, version: ProjectVersion, data_entity_lists: DataEntityLists,
-                      filter_non_compilable_versions: bool) -> bool:
-        if filter_non_compilable_versions:
-            return not version.is_compilable
-
+    def __is_filtered(self, project_id: str, version: ProjectVersion, data_entity_lists: DataEntityLists) -> bool:
         version_id = version.id
         white_list = data_entity_lists.get_version_white_list(project_id)
         black_list = data_entity_lists.black_list
 
         is_whitelisted = not white_list or version_id in white_list
         is_blacklisted = version_id in black_list
+        is_not_compilable = self._filter_non_compilable_versions and not version.is_compilable
 
-        return is_blacklisted or not is_whitelisted
+        return is_not_compilable or is_blacklisted or not is_whitelisted
