6aa8e897b0a86051ed00dc2a5fabfcdfbf71c27e
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-01-30 10:49:13 +0100

    Fix Jigsaw compile.

---
+++ data/jigsaw/versions/205/compile/Jigsaw/src/classes/sun/tools/javac/Main.java
@@ -0,0 +1,11 @@
+package sun.tools.javac;
+
+import java.io.OutputStream;
+
+public class Main {
+    public Main(OutputStream os, String s){}
+
+    public boolean compile(String[] args) {
+        return true;
+    }
+}
