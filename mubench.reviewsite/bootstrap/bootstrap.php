d491707cf7ec3e8b4ed25969ab10813524931d0e
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2018-01-30 15:33:29 +0100

    redirect to setup if no experiments table exists.

--- mubench.reviewsite/bootstrap/bootstrap.php
+++ mubench.reviewsite/bootstrap/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
+
 $container = $app->getContainer();
 
 require __DIR__ . '/logger.php';
@@ -9,3 +11,8 @@ require __DIR__ . '/db.php';
 require __DIR__ . '/auth.php';
 
 require __DIR__ . '/renderer.php';
+
+$exp = new \MuBench\ReviewSite\Models\Experiment;
+if(!Schema::hasTable($exp->getTable())){
+    header('Location: '.'/../setup/setup.php');
+}
