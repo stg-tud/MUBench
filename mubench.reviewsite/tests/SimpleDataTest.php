bff3d16f6fac1a498cbbcfbccfb65a5e4c41b456
Jonas Schlitzer <jonas_werner.schlitzer@stud.tu-darmstadt.de>
2018-01-31 14:22:14 +0100

    Resolve #242: Add test for the simple-test-data script (#261)

---
+++ mubench.reviewsite/tests/SimpleDataTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+
+require_once 'SlimTestCase.php';
+
+class SimpleDataTest extends SlimTestCase
+{
+
+    function test_simple_test_data_script()
+    {
+        require __DIR__ . '/../setup/create_database_tables.php';
+        $app = $this->app;
+        $container = $this->app->getContainer();
+        $capsule = Schema::getFacadeApplication()['db'];
+        require 'create_simple_test_data.php';
+    }
+
+}
