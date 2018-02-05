f6234bb28c1b7e8761bacd50b7e9e8e9a520563e
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-05 13:40:41 +0100

    Use absolute paths to create E4 index.

--- mubench.pipeline/crossproject_create_index.py
+++ mubench.pipeline/crossproject_create_index.py
@@ -1,4 +1,4 @@
-from typing import List
+import os
 
 from data.misuse import Misuse
 from data.project import Project
@@ -10,15 +10,25 @@ from tasks.task_runner import TaskRunner
 from utils.data_entity_lists import DataEntityLists
 from utils.dataset_util import get_white_list
 
+DATASET_NAME = "FSE18-Extension"
 
-class Task:
-    def run(self, project: Project, version: ProjectVersion, misuse: Misuse) -> List[str]:
+MUBENCH_ROOT_PATH = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+MUBENCH_DATA_PATH = os.path.join(MUBENCH_ROOT_PATH, "data")
+MUBENCH_DATASETS_FILE = os.path.join(MUBENCH_DATA_PATH, "datasets.yml")
+
+
+class PrintIndexTask:
+    def run(self, project: Project, version: ProjectVersion, misuse: Misuse):
         print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(project.id, version.version_id, misuse.misuse_id,
                                                   ':'.join(version.source_dirs),
                                                   misuse.location.file, misuse.location.method, misuse.apis[0]))
 
 
-white_list = get_white_list("../data/datasets.yml", "icse16ex1")
+white_list = get_white_list(MUBENCH_DATASETS_FILE, DATASET_NAME)
 initial_parameters = [DataEntityLists(white_list, [])]
-runner = TaskRunner([CollectProjectsTask("../data/"), CollectVersionsTask(False), CollectMisusesTask(), Task()])
-runner.run(*initial_parameters)
+TaskRunner([
+    CollectProjectsTask(MUBENCH_DATA_PATH),
+    CollectVersionsTask(False),
+    CollectMisusesTask(),
+    PrintIndexTask()
+]).run(*initial_parameters)
