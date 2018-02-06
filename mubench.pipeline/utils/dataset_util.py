19f4aa4a9045abfca5b1db4e58e369a1a4be86fd
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2018-02-06 10:07:59 +0100

    Lookup datasets case insensitively

--- mubench.pipeline/utils/dataset_util.py
+++ mubench.pipeline/utils/dataset_util.py
@@ -8,15 +8,25 @@ def get_available_datasets(datasets_file_path: str) -> Dict[str, List[str]]:
 
 
 def get_available_dataset_ids(datasets_file_path: str) -> List[str]:
-    datasets = read_yaml(datasets_file_path)
-    return list(datasets.keys())
+    datasets = __get_case_insensitive_datasets(datasets_file_path)
+    return datasets.keys()
 
 
 def get_white_list(datasets_file_path: str, dataset_key: Optional[str]) -> List[str]:
     if dataset_key is None:
         return []
 
-    datasets = read_yaml(datasets_file_path)
-    if dataset_key not in datasets:
+    datasets = __get_case_insensitive_datasets(datasets_file_path)
+    if dataset_key.lower() not in datasets:
         raise ValueError("Invalid dataset: '{}'".format(dataset_key))
     return datasets[dataset_key]
+
+
+def __get_case_insensitive_datasets(datasets_file_path: str) -> Dict[str, List[str]]:
+    datasets = read_yaml(datasets_file_path)
+    case_insensitive_datasets = dict()
+
+    for k, v in datasets.items():
+        case_insensitive_datasets[k.lower()] = v
+
+    return case_insensitive_datasets
