4d3b7590255817eb203f8b086f2ad5a09fa019b0
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-12 11:08:05 +0100

    Limit DOT-graph conversion to at most 15min per graph.

--- mubench.pipeline/data/detector_specialising/specialising_util.py
+++ mubench.pipeline/data/detector_specialising/specialising_util.py
@@ -1,9 +1,11 @@
 from base64 import urlsafe_b64encode
 from os import makedirs, remove
-from os.path import join, exists
+from os.path import join, exists, dirname
 
 from utils.io import safe_write
 from utils.shell import Shell
+
+__DOT_CONVERSION_TIME_LIMIT = 900
 
 
 def format_float_value(finding, float_key):
@@ -12,9 +14,14 @@ def format_float_value(finding, float_key):
 
 def replace_dot_graph_with_image(finding, key, base_path) -> str:
     image_name = "{}-{}.svg".format(__get_id(finding), __filename_encode(key))
-    __create_image(finding[key], base_path, image_name)
-    finding[key] = image_name
-    return join(base_path, image_name)
+    try:
+        __create_image(finding[key], base_path, image_name)
+        finding[key] = image_name
+        return join(base_path, image_name)
+    except TimeoutError:
+        finding[key] = "Generating DOT image was aborted, since it took longer than {} minutes. The resulting graph " \
+                       "would likely be unreadable anyways...".format(__DOT_CONVERSION_TIME_LIMIT / 60)
+        return join(dirname(__file__), "empty_dummy_dot.png")
 
 
 def __get_id(finding):
@@ -34,5 +41,5 @@ def __create_image(dot_graph, working_directory, image_name):
         makedirs(working_directory, exist_ok=True)
         dot_path = image_path + ".dot"
         safe_write(dot_graph, dot_path, append=False)
-        Shell.exec("dot -v -Tsvg -o'{}' '{}'".format(image_path, dot_path))
+        Shell.exec("dot -v -Tsvg -o'{}' '{}'".format(image_path, dot_path), timeout=__DOT_CONVERSION_TIME_LIMIT)
         remove(dot_path)
