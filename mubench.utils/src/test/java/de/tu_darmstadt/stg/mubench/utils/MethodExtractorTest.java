752be5b31e34945ab227f2c2bb014875f96d0436
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2018-02-16 10:56:44 +0100

    Remove obsolete import

--- mubench.utils/src/test/java/de/tu_darmstadt/stg/mubench/utils/MethodExtractorTest.java
+++ mubench.utils/src/test/java/de/tu_darmstadt/stg/mubench/utils/MethodExtractorTest.java
@@ -8,7 +8,6 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class MethodExtractorTest {
