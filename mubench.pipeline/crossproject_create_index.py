ad2a227bf8c994882b897ed07a96bad1ec50fa09
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-20 08:16:33 +0100

    Take dataset as input and write index to file.

--- mubench.pipeline/crossproject_create_index.py
+++ mubench.pipeline/crossproject_create_index.py
@@ -1,5 +1,7 @@
 import os
 
+import sys
+
 from data.misuse import Misuse
 from data.project import Project
 from data.project_version import ProjectVersion
@@ -10,11 +12,11 @@ from tasks.task_runner import TaskRunner
 from utils.data_entity_lists import DataEntityLists
 from utils.dataset_util import get_white_list
 
-DATASET_NAME = "FSE18-Extension".lower()
 
-MUBENCH_ROOT_PATH = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
-MUBENCH_DATA_PATH = os.path.join(MUBENCH_ROOT_PATH, "data")
-MUBENCH_DATASETS_FILE = os.path.join(MUBENCH_DATA_PATH, "datasets.yml")
+__MUBENCH_ROOT_PATH = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
+__MUBENCH_DATA_PATH = os.path.join(__MUBENCH_ROOT_PATH, "data")
+__MUBENCH_DATASETS_FILE = os.path.join(__MUBENCH_DATA_PATH, "datasets.yml")
+_INDEX_PATH = os.path.join(__MUBENCH_ROOT_PATH, "checkouts", "_examples", "index.csv")
 
 
 class PrintIndexTask:
@@ -22,14 +24,18 @@ class PrintIndexTask:
         print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(project.id, version.version_id, misuse.misuse_id,
                                                   ':'.join(version.source_dirs),
                                                   misuse.location.file, misuse.location.method,
-                                                  "\t".join(misuse.apis)))
+                                                  "\t".join(misuse.apis)), file=open(_INDEX_PATH, "a"))
 
 
-white_list = get_white_list(MUBENCH_DATASETS_FILE, DATASET_NAME)
+dataset = sys.argv[1].lower()
+
+white_list = get_white_list(__MUBENCH_DATASETS_FILE, dataset)
 initial_parameters = [DataEntityLists(white_list, [])]
-TaskRunner([
-    CollectProjectsTask(MUBENCH_DATA_PATH),
-    CollectVersionsTask(False),
-    CollectMisusesTask(),
-    PrintIndexTask()
-]).run(*initial_parameters)
+
+runner = TaskRunner(
+    [CollectProjectsTask(__MUBENCH_DATA_PATH), CollectVersionsTask(False), CollectMisusesTask(), PrintIndexTask()])
+
+if os.path.exists(_INDEX_PATH):
+    os.remove(_INDEX_PATH)
+
+runner.run(*initial_parameters)
