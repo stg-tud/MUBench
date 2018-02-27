70de71c8a710f4f1e2feab241298cba161d7a552
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2018-02-27 18:51:11 +0100

    Move redirect to button in setup script.

--- mubench.reviewsite/setup/setup.php
+++ mubench.reviewsite/setup/setup.php
@@ -13,5 +13,9 @@ $container = $app->getContainer();
 require __DIR__ . '/../bootstrap/db.php';
 
 require __DIR__ . '/../setup/create_database_tables.php';
+?>
+<link rel="stylesheet" type="text/css" href="../css/style.css"/>
+<p style="font-weight: bold">
+    Setup finished, <a class="button" href="..">click here</a> to return.
+</p>
 
-header('Location: ..');
