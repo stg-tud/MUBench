3363320dac8847294a547499c4920bf0a54704f2
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-27 08:30:54 +0100

    Fix XP prepare for single-type misuses.

--- mubench.pipeline/crossproject_prepare.py
+++ mubench.pipeline/crossproject_prepare.py
@@ -46,9 +46,9 @@ def _get_type_and_subtypes_list(target_type):
 
 def _create_type_combinations(target_types: List):
     if len(target_types) == 1:
-        return _get_type_and_subtypes_list(target_types[0])
+        return [_get_type_and_subtypes_list(target_types[0])]
     else:
-        return ((target_type, tail)
+        return ([target_type] + tail
                 for target_type in _get_type_and_subtypes_list(target_types[0])
                 for tail in _create_type_combinations(target_types[1:]))
 
