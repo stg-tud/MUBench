2923537c9ed0f8fe4b2bd925c05a547db6c28dce
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-02-28 09:50:46 +0100

    Release mubench.utils 0.0.3 with BOA update.

--- mubench.pipeline/utils/java_utils.py
+++ mubench.pipeline/utils/java_utils.py
@@ -6,11 +6,11 @@ from urllib.error import URLError
 from utils.shell import Shell
 from utils.web_util import is_valid_file, download_file
 
-__UTILS_VERSION = "0.0.2"
+__UTILS_VERSION = "0.0.3"
 __UTILS_JAR_NAME = "mubench.utils-{}-jar-with-dependencies.jar".format(__UTILS_VERSION)
 __UTILS_JAR_URL = "http://www.st.informatik.tu-darmstadt.de/artifacts/mubench/mvn/" \
                   "de/tu-darmstadt/stg/mubench/mubench.utils/{}/{}".format(__UTILS_VERSION, __UTILS_JAR_NAME)
-__UTILS_JAR_MD5 = "8b16eae8371bb563bc6da9805f8afd96"
+__UTILS_JAR_MD5 = "a21bc8f03aca09d348f267bc390e1dbc"
 
 
 def exec_util(main: str, args: str = "", timeout: Optional[int] = None):
