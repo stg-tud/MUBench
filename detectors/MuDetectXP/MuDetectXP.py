f8d21837274f1cc282692dab4ddc4fe346ba711c
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-03-02 14:25:08 +0100

    Fix #315: Add FSE18 versions of MUDetect/MUDetectXP.

--- detectors/MuDetectXP/MuDetectXP.py
+++ detectors/MuDetectXP/MuDetectXP.py
@@ -3,7 +3,7 @@ from data.detector_specialising.specialising_util import replace_dot_graph_with_
 from data.finding import Finding, SpecializedFinding
 
 
-class MuDetectXP(Detector):  # Commit: 347d8b887e2538d1138c951a40b247324657a6ad
+class MuDetectXP(Detector):
     def specialize_finding(self, findings_path: str, finding: Finding) -> SpecializedFinding:
         violation = replace_dot_graph_with_image(finding, "pattern_violation", findings_path)
         target_env = replace_dot_graph_with_image(finding, "target_environment_mapping", findings_path)
