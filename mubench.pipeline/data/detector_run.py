f437e75de265e4d7c2e9a7a3a587f02d95107b61
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2018-03-20 08:50:51 +0100

    Implement #291: Output start time and detector when starting run (#332)

--- mubench.pipeline/data/detector_run.py
+++ mubench.pipeline/data/detector_run.py
@@ -85,7 +85,7 @@ class DetectorRun:
 
         self.reset()
 
-        logger.info("Executing %s...", self)
+        logger.info("Running '%s' on %s ... (%s)", self.detector, self.version, time.strftime("%H:%M"))
 
         detector_args.update({
             key_findings_file: self._findings_file_path,
