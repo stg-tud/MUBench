3a89a7963dc3d19d6b78b1bdad11d6b7096661d8
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2018-03-28 10:39:10 +0200

    Warn if all misuses were filtered

--- mubench.pipeline/tasks/implementations/collect_misuses.py
+++ mubench.pipeline/tasks/implementations/collect_misuses.py
@@ -1,11 +1,19 @@
+import logging
+
 from data.project_version import ProjectVersion
 from utils.data_entity_lists import DataEntityLists
 
 
 class CollectMisusesTask:
     def run(self, version: ProjectVersion, data_entity_lists: DataEntityLists):
-        return [misuse for misuse in version.misuses
-                if not self.__is_filtered(version.id, misuse.id, data_entity_lists)]
+        misuses = [misuse for misuse in version.misuses if
+                   not self.__is_filtered(version.id, misuse.id, data_entity_lists)]
+
+        if not misuses:
+            logger = logging.getLogger("tasks.collect_misuses")
+            logger.warning("Filtered all misuses of {}!".format(version))
+
+        return misuses
 
     @staticmethod
     def __is_filtered(version_id: str, misuse_id: str, data_entity_lists: DataEntityLists) -> bool:
