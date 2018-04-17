b2485534275248726beb79ec0e8b339b1e11deec
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2018-04-17 12:39:15 +0200

    Minor.

--- mubench.reviewsite/src/Controllers/RunsController.php
+++ mubench.reviewsite/src/Controllers/RunsController.php
@@ -366,7 +366,7 @@ class RunsController extends Controller
             $table->dateTime('created_at');
             $table->dateTime('updated_at');
             $table->index(['misuse_id']);
-            $table->unique(['experiment_id', 'project_muid', 'version_muid', 'misuse_muid', 'rank'],"findings_id_{$table_name}");
+            $table->unique(['experiment_id', 'project_muid', 'version_muid', 'misuse_muid', 'rank'], "findings_id_{$table_name}");
         });
     }
 
