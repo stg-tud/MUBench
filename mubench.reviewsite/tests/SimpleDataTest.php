8eb5ebdac73612d53e6fa6c9a0083a4935a99d51
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2018-04-18 17:09:00 +0200

    Abstract db setup script from default db connection.

--- mubench.reviewsite/tests/SimpleDataTest.php
+++ mubench.reviewsite/tests/SimpleDataTest.php
@@ -9,6 +9,7 @@ class SimpleDataTest extends SlimTestCase
 
     function test_simple_test_data_script()
     {
+        $schema = Schema::connection('default');
         require __DIR__ . '/../setup/create_database_tables.php';
         $app = $this->app;
         $container = $this->app->getContainer();
