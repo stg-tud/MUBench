45cd45518feca008fdb4aeb2d0ff14ce3d403c2b
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2018-05-02 16:23:00 +0200

    Remove formatter

--- mubench.docs/browse.py
+++ mubench.docs/browse.py
@@ -4,17 +4,10 @@ from operator import attrgetter
 import sys
 
 
-class SortingHelpFormatter(HelpFormatter):
-    def add_arguments(self, actions):
-        actions = sorted(actions, key=attrgetter('option_strings'))
-        super(SortingHelpFormatter, self).add_arguments(actions)
-
-
 parser = ArgumentParser(
     prog="./mubench browse",
     description="Browse MUBench docker volumes.",
     epilog="For details, check out "
-           "https://github.com/stg-tud/MUBench/tree/master/mubench.pipeline#experiment-data.",
-    formatter_class=SortingHelpFormatter)
+           "https://github.com/stg-tud/MUBench/tree/master/mubench.pipeline#experiment-data.")
 
 parser.parse_args(sys.argv)
