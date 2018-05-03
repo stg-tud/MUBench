752321b8bf0bcf7b2c37b9a4a222939682cbc79f
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2018-05-03 12:50:39 +0200

    Fix snippet posting with detector_muid.

--- mubench.reviewsite/src/Controllers/SnippetsController.php
+++ mubench.reviewsite/src/Controllers/SnippetsController.php
@@ -14,13 +14,14 @@ class SnippetsController extends Controller
     public function postSnippet(Request $request, Response $response, array $args)
     {
         $form = $request->getParsedBody();
+        $detectorId = $args['detector_muid'];
         $projectId = $args['project_muid'];
         $versionId = $args['version_muid'];
         $misuseId = $args['misuse_muid'];
         $code = $form['snippet'];
         $line = $form['line'];
         $misuse = Misuse::find($form['misuse_id']);
-        self::createSnippetIfNotExists($projectId, $versionId, $misuseId, $misuse->getFile(), $line, $code);
+        self::createSnippetIfNotExists($projectId, $versionId, $misuseId, $misuse->getFile(), $line, $code, $detectorId);
         return $response->withRedirect($form['path']);
     }
 
