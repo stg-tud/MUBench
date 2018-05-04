7e6c36b35b4cfd0cdde7feed2a00769feeea957d
Mattis Manfred Kämmerer <mattis.kaemmerer@gmx.de>
2018-05-04 09:08:12 +0200

    Add help logic for batch files

--- mubench.bat
+++ mubench.bat
@@ -1,14 +1,24 @@
-@ECHO OFF
-
 SET SCRIPT_DIR=%~dp0
 SET MUBENCH_ROOT=%SCRIPT_DIR%
 SET TASKS=%MUBENCH_ROOT%mubench.bin\
 
 CALL %TASKS%environment.bat
 
+IF "%~1" == "-h" (
+  docker run --rm -v "%MUBENCH_ROOT%":/mubench %PIPELINE_DOCKER_IMAGE% python3 %MUBENCH_DOCS%/mubench.py %*
+  EXIT 0
+)
+
 SET TASK=%TASKS%%~1.bat
+SET DOC=%MUBENCH_DOCS%%~1.py
 IF EXIST %TASK% (
   SHIFT
+  IF %~1 == "-h" (
+    IF EXIST %DOC% (
+      docker run --rm -v "%MUBENCH_ROOT%":/mubench %PIPELINE_DOCKER_IMAGE% python3 %DOC% %*
+	  EXIT 0
+	)
+  )
   CALL %TASK% %*
 ) ELSE (
   CALL %TASKS%pipeline.bat %*
