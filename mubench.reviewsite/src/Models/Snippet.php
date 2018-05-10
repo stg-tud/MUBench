c51ec3b3ff086d3b944048de8144263f97c79b58
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2018-05-10 11:19:07 +0200

    reverse formatting.

--- mubench.reviewsite/src/Models/Snippet.php
+++ mubench.reviewsite/src/Models/Snippet.php
@@ -16,8 +16,10 @@ class Snippet extends Model
     }
 
     public function scopeOf($query, $projectId, $versionId, $misuseId, $file) {
-        return $query->where(['project_muid'=> $projectId, 'version_muid' => $versionId, 'misuse_muid' => $misuseId,
-            'file' => self::hashFile($file)]);
+        return $query->where([
+            'project_muid'=> $projectId, 'version_muid' => $versionId, 'misuse_muid' => $misuseId,
+            'file' => self::hashFile($file)
+        ]);
     }
 
     public static function createIfNotExists($projectId, $versionId, $misuseId, $file, $line, $code, $detectorId = "")
