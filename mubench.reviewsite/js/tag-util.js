f2716ac76ab75e0a691b071ccc9cbd76502e78df
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2018-05-15 16:53:21 +0200

    Fix removeTag.

--- mubench.reviewsite/js/tag-util.js
+++ mubench.reviewsite/js/tag-util.js
@@ -7,7 +7,7 @@ function addTag(url, misuseId, tagName){
             };
             postJson(url, data, function(r){
                 let tag = JSON.parse(r.srcElement.responseText);
-                misuseTagsDiv.innerHTML += getTagTemplate(tagName, tag);
+                misuseTagsDiv.innerHTML += getTagTemplate(misuseId, tagName, tag);
             });
         }
         event.target.value = "";
@@ -17,18 +17,18 @@ function isExistingTag(misuseTagsDiv, tagName){
     return misuseTagsDiv.querySelectorAll("span#" + tagName).length == 0;
 }
 
-function removeTag(url, button){
+function removeTag(url, misuseId, tagElement){
     postJson(url, {"misuse_id": misuseId}, (r) => {
         let misuseTagsDiv = document.getElementById('misuse-tags');
-        misuseTagsDiv.removeChild(button.parentElement);
+        misuseTagsDiv.removeChild(tagElement);
     });
 }
 
-function getTagTemplate(tagName, tag){
+function getTagTemplate(misuseId, tagName, tag){
     let template = `
         <div class="misuse-tag" style="background-color:${tag.color};color:${tag.fontColor};">
             <span id="${tagName}">${tagName}</span>
-            <button onclick="removeTag('${tag.removeUrl}', this)" style="border:none;outline:none;margin:0;padding:0;background-color:${tag.color}"><i class="fa fa-trash"></i></button>
+            <button onclick="removeTag('${tag.removeUrl}', '${misuseId}', this.parentElement)" style="border:none;outline:none;margin:0;padding:0;background-color:${tag.color}"><i class="fa fa-trash"></i></button>
         </div>`;
     return template;
 }
