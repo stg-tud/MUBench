3b90a85038a6a84df0669d24640004c823b899bc
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2018-05-22 15:57:05 +0200

    Add simple Webdriver test

---
+++ mubench.reviewsite/tests/WebDriverTest.php
@@ -0,0 +1,46 @@
+<?php
+
+use Facebook\WebDriver\Remote\DesiredCapabilities;
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use PHPUnit\Framework\TestCase;
+
+class WebDriverTest extends TestCase
+{
+    /**
+     * @var RemoteWebDriver
+     */
+    protected $driver;
+
+    public function setUp()
+    {
+        $chromeOptions = new \Facebook\WebDriver\Chrome\ChromeOptions();
+        $chromeOptions->addArguments(array('headless'));
+        $capabilities = DesiredCapabilities::chrome();
+        $capabilities->setCapability(\Facebook\WebDriver\Chrome\ChromeOptions::CAPABILITY, $chromeOptions);
+        $this->driver = RemoteWebDriver::create("http://localhost:4444/wd/hub",
+            $capabilities, 5000
+        );
+    }
+
+    public function tearDown()
+    {
+        $this->driver->quit();
+    }
+
+    public function testAssertTitle()
+    {
+        $this->driver->get('http://localhost:8080');
+        self::assertEquals("MUBench Review Site", $this->driver->getTitle());
+    }
+
+    public function testLoginUser()
+    {
+        $this->driver->get('http://admin:pass@localhost:8080/private/');
+        $span = $this->driver->findElement(
+            WebDriverBy::tagName('span')::className("right ")
+        );
+        $headlines = array("Reviewer: admin");
+        self::assertEquals("Reviewer: admin", $span->getText());
+    }
+}
