4f1121587e6a54a748cfb46ae743e08edd6d9470
Jonas Schlitzer <jonas.schlitzer@googlemail.com>
2018-06-14 09:11:23 +0200

    Remove run script.

--- mubench.reviewsite/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -jar /usr/local/bin/selenium-server-standalone-3.12.0.jar &> /dev/null &
-php -S localhost:8080 -t /mubench/mubench.reviewsite &> /dev/null &
-phpunit --bootstrap vendor/autoload.php tests/
