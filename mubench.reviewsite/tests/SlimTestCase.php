c262ab4c9cae18e8c0708f4735026fb93c482420
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-06-19 10:53:09 +0200

    Remove obsolete helper method.

--- mubench.reviewsite/tests/SlimTestCase.php
+++ mubench.reviewsite/tests/SlimTestCase.php
@@ -144,40 +144,4 @@ class SlimTestCase extends TestCase
         $sqlite = str_replace(" ENGINE=MyISAM  DEFAULT CHARSET=latin1;", ";", $sqlite);
         return $sqlite;
     }
-
-    protected function createFindingWith($experiment, $detector, $misuse)
-    {
-        $finding = new \MuBench\ReviewSite\Models\Finding;
-        $finding->setDetector($detector);
-        Schema::dropIfExists($finding->getTable());
-        if(!Schema::hasTable($finding->getTable())){
-            Schema::create($finding->getTable(), function (Blueprint $table) {
-                $table->increments('id');
-                $table->integer('experiment_id');
-                $table->integer('misuse_id');
-                $table->string('project_muid', 30);
-                $table->string('version_muid', 30);
-                $table->string('misuse_muid', 30);
-                $table->integer('startline');
-                $table->integer('rank');
-                $table->integer('additional_column')->nullable();
-                $table->text('file');
-                $table->text('method');
-                $table->dateTime('created_at');
-                $table->dateTime('updated_at');
-            });
-        }
-
-        $finding->experiment_id = $experiment->id;
-        $finding->misuse_id = $misuse->id;
-        $finding->project_muid = 'mubench';
-        $finding->version_muid = '42';
-        $finding->misuse_muid = '0';
-        $finding->startline = 113;
-        $finding->rank = 1;
-        $finding->file = 'Test.java';
-        $finding->method = "method(A)";
-        $finding->save();
-    }
-
 }
