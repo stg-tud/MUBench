a6782cfc1b672051b320ce4141162f7eba74176f
Sven Amann <amann@st.informatik.tu-darmstadt.de>
2018-09-20 15:21:23 +0200

    Provide help command.

--- mubench.docs/mubench.py
+++ mubench.docs/mubench.py
@@ -1,26 +1,43 @@
-from argparse import HelpFormatter, ArgumentParser
+from argparse import HelpFormatter, ArgumentParser, _SubParsersAction, SUPPRESS
 from operator import attrgetter
 
-import sys
+import sys, os
 
-
-__TASKS = ['pipeline', 'browse', 'reviewsite', 'update']
-
-
-class SortingHelpFormatter(HelpFormatter):
-    def add_arguments(self, actions):
-        actions = sorted(actions, key=attrgetter('option_strings'))
-        super(SortingHelpFormatter, self).add_arguments(actions)
-
+class CustomHelpFormatter(HelpFormatter):
+    def _format_action(self, action):
+        if type(action) == _SubParsersAction:
+            msg = ''
+            for subaction in action._get_subactions():
+                msg += self._format_action(subaction)
+            return msg
+        else:
+            return super(CustomHelpFormatter, self)._format_action(action)
 
 parser = ArgumentParser(
-    prog="./mubench",
-    description="Run MUBench, the benchmark for API-misuse detectors.",
-    epilog="For details, check out https://github.com/stg-tud/MUBench.",
-    formatter_class=SortingHelpFormatter)
+    usage=SUPPRESS,
+    description="This is the MUBench interactive shell.",
+    epilog="For details, check out `<command> -h` or https://github.com/stg-tud/MUBench.",
+    formatter_class=CustomHelpFormatter)
 
-parser.add_argument('task', nargs='?', choices=__TASKS, default='pipeline',
-                    help="MUBench supports several tasks. "
-                         "Run `mubench <task> -h` for details.")
+parser._positionals.title = "Control MUBench via the following commands"
+subparsers = parser.add_subparsers(dest='command')
+
+subparsers.add_parser("pipeline",
+                      help="Run benchmarking experiments, including checkout "
+                           "and compilation of target projects, execution of "
+                           "detectors, and publishing data to a review site.")
+subparsers.add_parser("reviewsite",
+                      help="Fire up a review site inside this container. The "
+                           "site is available from the host system via "
+                           "`http://localhost:8080/`.")
+subparsers.add_parser("debug",
+                      help="Run benchmark experiments for remote debugger of "
+                           "of a detector from the host system.")
+subparsers.add_parser("update",
+                      help="Update MUBench.")
+
+if len(sys.argv) == 1:
+    parser.print_help()
+    sys.exit(0)
 
 parser.parse_args(sys.argv[1:])
