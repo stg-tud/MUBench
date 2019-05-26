0803d097ead9464b0a55bc063735d278df4808d4
Anna-Katharina Wickert <wickert@st.informatik.tu-darmstadt.de>
2019-05-26 10:04:30 -0400

    Fix jeesuite-libs
    mubench> pipeline compile --only jeesuite-libs
    [INFO   ] Starting benchmark...
    [INFO   ] All requirements satisfied. You're good to go.
    [INFO   ] Correct usage already compiled.
    [INFO   ] Correct usage already compiled.
    [INFO   ] Compiling correct usage for misuse 'jeesuite-libs.2a545bd.3'...
    [INFO   ] Correct usage already compiled.
    [INFO   ] Correct usage already compiled.
    [INFO   ] Correct usage already compiled.
    [INFO   ] Correct usage already compiled.
    [INFO   ] Correct usage already compiled.
    [INFO   ] Correct usage already compiled.

--- data/jeesuite-libs/misuses/4/correct-usages/DES.java
+++
@@ -1,9 +0,0 @@
-import javax.crypto.spec.IvParameterSpec;
-import java.security.SecureRandom;
-
-ConstructIvParameterSpec {
-	pattern() {
-		SecureRandom iv = SecureRandom(); 
-		IvParameterSpec ips = new IvParameterSpec(iv);
-	}
-}
